d5c1f575928380c78f718d7c81c49b61108b8faa
Zhang Shengli <1103137864@qq.com>
2022-11-05 10:21:02 +0800

    MUX sim

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,10 @@
-module top(
-	input a,
-	input b,
-	output f
-);
-        assign f=a^b;
+module top(X0,X1,X2,X3,F,Y);
+  input   [1:0] X0,X1,X2,X3,Y;
+  output  [1:0] F;
+  MuxKey #(4, 2, 2) i0 (F, Y, {
+    2'b00, X0,
+    2'b01, X1,
+    2'b10, X2,
+    2'b11, X3
+    });
 endmodule
