27be52d697e100e491e6803b0f447f2ff8a471c5
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 15:37:20 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:20 up 34 min,  3 users,  load average: 0.03, 0.04, 0.08

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -29,9 +29,9 @@ void sim_exit(){
 int main() {
   sim_init();
 
-  top->mode=0; top->A=7; top->B=4;  step_and_dump_wave();   // 将s，a和b均初始化为“0”
-                      top->B=7;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，继续保持“0”，
-            top->A=1; top->B=2;  step_and_dump_wave();   // 将a，b分别改为“1”和“0”，s的值不变，
+  top->mode=0; top->A=7; top->B=4;  step_and_dump_wave();  
+                      top->B=7;  step_and_dump_wave();   
+            top->A=1; top->B=2;  step_and_dump_wave();  
                       top->B=6;  step_and_dump_wave();   
   top->mode=1; top->A=0; top->B=8;  step_and_dump_wave();   
                       top->B=10;  step_and_dump_wave();
