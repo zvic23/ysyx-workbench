a44bec4a3748321b8ffa1c468ae91f0ae37b0d6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 19:44:26 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:44:26 up 13 min,  3 users,  load average: 0.23, 0.32, 0.35

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -190,7 +190,10 @@ bool check_parentheses(int p, int q){
 	if((tokens[p].type != '(')||(tokens[q].type != ')')){
 		printf("check_parentheses fail!!!\n");
 	//	assert(0);
-        for(int j=p+1;j<q;j++){
+	        return false;
+	}
+	else{ 
+		for(int j=p+1;j<q;j++){
 	       if(tokens[j].type == '('){
 		       match++;
 	       }
@@ -203,8 +206,8 @@ bool check_parentheses(int p, int q){
 		       return false;
 	       }
 	}
-	}
         return true;
+	}
 }
 
 int eval(int p, int q){
