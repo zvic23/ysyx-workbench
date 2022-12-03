c23f297cb98371ce3a01c833102c7ad62f38d7df
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-03 19:55:30 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:30 up 51 min,  5 users,  load average: 0.04, 0.16, 0.12

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -46,7 +46,7 @@ static struct rule {
   {"\\(",  '('},        // left bracket
   {"\\)",  ')'},        // right bracket
   {"[0-9]+", TK_NUMBER},// number
-  {"0[xX][0-9a-f]+", TK_HEXNUMBER},   // hex-number
+  {"0x[[:digit:][:lower:]]+", TK_HEXNUMBER},   // hex-number
 			
   {"\\\n", TK_NEXTLINE},// next line
 };
