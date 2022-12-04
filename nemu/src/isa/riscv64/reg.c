19219b83063041f905820f4f994778cb2b91310e
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 09:56:52 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:56:52 up 54 min,  5 users,  load average: 0.84, 0.69, 0.46

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -37,10 +37,11 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 	uint64_t value = 0;
 	char reg_name[4];
 	sprintf(reg_name ,"%s",s);
-	char *name;
-//	name[0]=reg_name[0];
-//	name[1]=reg_name[1];
-        name = "t0";
+	char name[2] ;
+        
+	name[0]=reg_name[0];
+	name[1]=reg_name[1];
+        //name = "t0";
 	int j =0;
 	//int i;
 
