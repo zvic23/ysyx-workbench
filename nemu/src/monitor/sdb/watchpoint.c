92bc80379ac909864f1df2f4e0b12179cb7b0fe0
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-06 20:26:08 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:26:08 up  1:41,  7 users,  load average: 0.17, 0.14, 0.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -93,8 +93,9 @@ void setwp(char *expression){
 void check_wpchange(){
 	WP *c = head;
 	for(c=head; c; c=c->next){
-		//evaluation(c->expression);
-		printf("wp:result=%s\n",c->expression);
+
+		evaluation(c->expression);
+		//printf("wp:result=%ld\n",number.value);
 	}
 }
 
