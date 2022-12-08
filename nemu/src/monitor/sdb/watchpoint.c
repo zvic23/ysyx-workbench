cd69c331d8f4e3100e7653717e16061a6f5e773a
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-08 10:52:35 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:34 up  1:38,  8 users,  load average: 0.06, 0.06, 0.07

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -112,7 +112,7 @@ int check_wpchange(){
 	for(c=head; c; c=c->next){
 		struct figure result =evaluation(c->expression);
 		if(result.sign != c->lastresult.sign || result.value != c->lastresult.value){
-			printf("watchpoint NO.%d (%s): %ld -> %ld    hex: 0x%lx -> 0x%lx\n",c->NO,c->expression,c->lastresult.value,result.value,c->lastresult.value,result.value);
+			printf("Watchpoint %d ( %s ): %ld -> %ld    hex: 0x%lx -> 0x%lx\n",c->NO,c->expression,c->lastresult.value,result.value,c->lastresult.value,result.value);
 			c->lastresult.sign = result.sign; 
 			c->lastresult.value = result.value; 
 			stop_exec = 1;
