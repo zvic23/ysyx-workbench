dcdc5dd18a4c51b4d91046500da8e985a70cce74
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 17:55:49 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:55:49 up  1:48,  4 users,  load average: 0.35, 0.14, 0.04

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -105,13 +105,13 @@ static int decode_exec(Decode *s) {
 	if(BITS(src1,31,31)!=BITS(src2,31,31))quotient=(quotient^0xffffffff)+1;//|0x80000000;
 	R(dest) = SEXT(quotient,32));               //!!!!!have doubt and to be optimizied 
 						    
-  INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, word_t src1_32=BITS(src1,31,0);word_t src2_32=BITS(src2,31,0);word_t quotient=0;word_t src1_32_cpl=(src1_32^0xffffffff)+1; word_t src2_32_cpl=(src2_32^0xffffffff)+1; 
-	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==0)quotient=src1_32%src2_32;
-	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==1)quotient=src1_32_cpl%src2_32_cpl;
-	if(BITS(src1,31,31)!=BITS(src2,31,31)&&BITS(src1,31,31)==0)quotient=src1_32%src2_32_cpl;
-	if(BITS(src1,31,31)!=BITS(src2,31,31)&&BITS(src1,31,31)==1)quotient=src1_32_cpl%src2_32;
-	if(BITS(src1,31,31)!=BITS(src2,31,31))quotient=(quotient^0xffffffff)+1;//|0x80000000;
-	R(dest) = SEXT(quotient,32));               //!!!!!have doubt and to be optimizied 
+  INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, word_t src1_32=BITS(src1,31,0);word_t src2_32=BITS(src2,31,0);word_t remainder=0;word_t src1_32_cpl=(src1_32^0xffffffff)+1; word_t src2_32_cpl=(src2_32^0xffffffff)+1; 
+	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==0)remainder=src1_32%src2_32;
+	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==1)remainder=src1_32_cpl%src2_32_cpl;
+	if(BITS(src1,31,31)!=BITS(src2,31,31)&&BITS(src1,31,31)==0)remainder=src1_32%src2_32_cpl;
+	if(BITS(src1,31,31)!=BITS(src2,31,31)&&BITS(src1,31,31)==1)remainder=src1_32_cpl%src2_32;
+	if(BITS(src1,31,31)!=BITS(src2,31,31))remainder=(remainder^0xffffffff)+1;//|0x80000000;
+	R(dest) = SEXT(remainder,32));               //!!!!!have doubt and to be optimizied    this inst is copy-paste 
 
 
 
