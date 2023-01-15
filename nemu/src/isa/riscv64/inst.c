f4533cedb73771885b5edbe053a7b3324c3fca5a
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 11:04:19 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:04:19 up  1:44,  5 users,  load average: 0.20, 0.11, 0.03

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -91,11 +91,10 @@ static int decode_exec(Decode *s) {
   INSTPAT("000000? ????? ????? 001 ????? 00100 11", slli   , I, R(dest) = src1 << BITS(imm, 5, 0));
   INSTPAT("000000? ????? ????? 101 ????? 00100 11", srli   , I, R(dest) = src1 >> BITS(imm, 5, 0));
   INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge    , B, src1=0x8000000000000000;src2=0x0000000000000001;word_t match=0;
-if(BITS(src1,63,63)==BITS(src2,63,63)&&BITS(src1,63,63)==0)match=(src1>=src2)?1:0;
-if(BITS(src1,63,63)==BITS(src2,63,63)&&BITS(src1,63,63)==1)match=(src1<=src2)?1:0;
-if(BITS(src1,63,63)!=BITS(src2,63,63))match=(BITS(src1,63,63)==0)?1:0;
-//if(match)s->dnpc=s->pc+imm;else s->dnpc=s->dnpc);
-s->dnpc = (match)?(s->pc+imm):s->dnpc);     //!!!!!todo
+	if(BITS(src1,63,63)==BITS(src2,63,63)&&BITS(src1,63,63)==0)match=(src1>=src2)?1:0;
+	if(BITS(src1,63,63)==BITS(src2,63,63)&&BITS(src1,63,63)==1)match=(src1<=src2)?1:0;
+	if(BITS(src1,63,63)!=BITS(src2,63,63))match=(BITS(src1,63,63)==0)?1:0;
+	s->dnpc = (match)?(s->pc+imm):s->dnpc);     //!!!!!todo
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
 
 
