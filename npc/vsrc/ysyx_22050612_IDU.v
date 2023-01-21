6351af4f4a926bde5a48f6cbbe231215ba1e8167
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 10:23:32 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:23:32 up 13 min,  7 users,  load average: 0.20, 0.26, 0.34

--- npc/vsrc/ysyx_22050612_IDU.v
+++ npc/vsrc/ysyx_22050612_IDU.v
@@ -25,7 +25,7 @@ assign imm_I= (inst[31]==1'b1)?{{52{1'b1}},inst[31:20]}:{{52{1'b0}},inst[31:20]}
 assign opcode=inst[0];
 
   always @(posedge clk) begin
-    $display("%x,%d,%d",inst,opcode,rd);
+    //$display("%x,%d,%d",inst,opcode,rd);
   end
 
 
