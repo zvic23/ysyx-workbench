6dda64af4e356819f1a1c1d52e330676ae51093b
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 12:42:59 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:42:59 up  2:32,  7 users,  load average: 0.01, 0.02, 0.00

--- npc/vsrc/ysyx_22050612_EXU.v
+++ npc/vsrc/ysyx_22050612_EXU.v
@@ -38,8 +38,8 @@ assign wdata = (opcode)? sum0:64'b0;
 //  });
 
   always @(posedge clk) begin
-    $display("%d,%d,%d",rd,rs1,imm_I);
-    //$display("%d,%d,%d,%d",sum0,wdata,wen,opcode);
+    //$display("%d,%d,%d",rd,rs1,imm_I);
+    $display("%d,%d,%d,%d",sum0,wdata,wen,opcode);
   end
 
 endmodule
