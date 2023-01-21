0d53a2005e21de80d77a7fce8abfa22a79b92504
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 15:51:37 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:51:37 up 7 min,  6 users,  load average: 0.14, 0.59, 0.42

--- npc/vsrc/ysyx_22050612_RegisterFile.v
+++ npc/vsrc/ysyx_22050612_RegisterFile.v
@@ -16,9 +16,9 @@ module ysyx_22050612_RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
 
   always @(posedge clk) begin
 
-    $display("%d,%d,%d",wdata,waddr,wen);
-	  if (wen) begin rf[waddr] <= wdata;
     //$display("%d,%d,%d",wdata,waddr,wen);
+	  if (wen) begin rf[waddr] <= wdata;
+    $display("%d,%d,%d",wdata,waddr,wen);
   end
   end
 
