4bd76c9142045d6cbd8d991069e4a16f9e52c865
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 11:32:51 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:32:51 up  2:04,  6 users,  load average: 0.07, 0.08, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -47,9 +47,9 @@ int main() {
   *(uint32_t*)&pmem[0x00000000]=0x00100093;
   *(uint32_t*)&pmem[0x00000004]=0x00208113;
   *(uint32_t*)&pmem[0x00000008]=0x00310193;
-  *(uint32_t*)&pmem[0x0000000c]=0x00418213;
+  //*(uint32_t*)&pmem[0x0000000c]=0x00418213;
 
-  *(uint32_t*)&pmem[0x00000010]=0x00100073; //ebreak
+  *(uint32_t*)&pmem[0x0000000c]=0x00100073; //ebreak
 
   sim_init();
 
