24901bef6de4a19eb5f709cf527ff58d8d650645
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-03 20:55:25 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:25 up  1:53,  6 users,  load average: 0.17, 0.09, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -63,7 +63,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  int *a = s;
+  for(int i=0; i<n; n++){
+	  a[i]=c;
+  }
+  return s;
+  //panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
