6add9e63e5e32586713a4aca15caa6e66134a2b8
Zhang Shengli <1103137864@qq.com>
2023-02-04 20:20:43 +0800

    （基础阶段：运行时环境和基础设施）完成了PA2.2“程序，运行时环境与AM”中的所有内容，即通过批处理模式运行NEMU，实现字符串处理函数和实现sprintf三部分内容。

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,51 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+const char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int sprintf(char *out, const char *fmt, ...) {     //to be completed    the integer of %d is nagetive haven't been concerned.   the value of return is nagetive when meet wrong 
+  int i = 0;
+  int j = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  while(fmt[i]!='\0'){
+	  if(fmt[i]=='%' && fmt[i+1]=='s'){
+		  char *s = va_arg(ap, char *);
+		  int length = strlen(s);
+		  for(int k=0;k<length;k++){
+			  out[j] = s[k];
+			  j++;
+		  }
+		  i=i+2;
+	  }
+	  else if(fmt[i]=='%' && fmt[i+1]=='d'){
+		  int number = va_arg(ap, int);
+		  int base = 10;
+		  char buff[20];
+		  char *a=buff;
+	          do
+                  {
+                          *a++ = digits[number % base];
+                          number /= base;
+                  } while (number);
+                  //if (!*result) *buff++ = '0';
+                  *a = '\0';
+		  int length = strlen(buff);
+		  for(int k=0;k<length;k++){
+			  out[j] = buff[length-1-k];
+			  j++;
+		  }
+		  i=i+2;
+	  }
+	  else {
+		  out[j]=fmt[i];
+		  i++;
+		  j++;
+	  }
+  }
+  out[j]='\0';
+  va_end(ap);
+  return j; 
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,26 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+  while(1){
+	  if(s[i] == '\0'){
+		  break;
+	  }
+	  i++;
+  } 
+  return i;
+  //panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t src_len = strlen(src);
+
+  for (size_t i = 0; i < src_len; i++){
+	  dst[i] = src[i];
+  }
+  dst[src_len] = '\0';
+  return dst;
+  //panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +32,30 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst);
+  size_t src_len = strlen(src);
+  
+  for (size_t i = 0; i < src_len; i++){
+	  dst[dst_len + i] = src[i];
+  }
+  dst[dst_len + src_len] = '\0';
+  return dst;
+  //panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i=0;
+  while(1){
+	  if(s1[i]==s2[i]&&s1[i]=='\0'){
+		  return 0;
+	  }
+	  if(s1[i]!=s2[i]){
+		  int result = s1[i]-s2[i];
+		  return result;
+	  }
+	  i++;
+  }
+  //panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +63,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *a = s;                      //zsl: char can be replaced by uint8_t
+  for(size_t i=0; i<n; i++){
+	  a[i]=c;
+  }
+  return s;
+  //panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +80,21 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if(n == 0){
+	  return 0;
+  }
+  else {
+	  const unsigned char *a1=s1;
+	  const unsigned char *a2=s2;
+	  for(size_t i = 0; i < n; i++){
+		  int result = a1[i] - a2[i];
+		  if(result != 0){
+			  return result;
+		  }
+	  }
+	  return 0;
+  }
+  //panic("Not implemented");
 }
 
 #endif
