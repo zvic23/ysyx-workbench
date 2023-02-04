156adcdbe37852a9708ef7f3ad9fc5e3be334d30
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 12:22:11 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:22:11 up  2:52,  4 users,  load average: 0.51, 0.54, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,39 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {     //to be completed
+  int i = 0;
+  int j = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  while(fmt[i]!='\0'){
+	  if(fmt[i]=='%' && fmt[i+1]=='s'){
+		  char *s = va_arg(ap, char *);
+		  int length = strlen(s);
+		  for(int k=0;k<length;k++){
+			  out[j] = s[k];
+			  j++;
+		  }
+		  i=i+2;
+	  }
+	  else if(fmt[i]=='%' && fmt[i+1]=='d'){
+		  int d = va_arg(ap, int);
+		  char *s = d + "";
+		  int length = strlen(s);
+		  for(int k=0;k<length;k++){
+			  out[j] = s[k];
+			  j++;
+		  }
+		  i=i+2;
+	  }
+	  else {
+		  out[j]=fmt[i];
+		  i++;
+		  j++;
+	  }
+  }
+  return j; 
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
