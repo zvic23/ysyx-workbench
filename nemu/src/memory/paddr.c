24e1ca7bd7a4eb98ffcd0462eb7318813dce2ea6
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-05 15:33:57 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:33:57 up 31 min,  3 users,  load average: 0.07, 0.06, 0.11

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -70,11 +70,11 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) {
 	  if(addr >= 0x80000100 && addr <= 0x80000110){
-	  	printf("mtrace:memory read addr:%x length:%d data:%lx\n",\
+	  	printf("mtrace:memory read   addr:%x length:%d data:%lx\n",\
 				addr,len,pmem_read(addr,len));
 	  }
 	  return pmem_read(addr, len);
-	  }
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
@@ -83,10 +83,11 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { 
 	  if(addr >= 0x80000100 && addr <= 0x80000110){
-	  	printf("mtrace:memory write addr:%x length:%d data:%lx\n",\
+	  	printf("mtrace:memory write   addr:%x length:%d data:%lx\n",\
 		addr,len,data);
 	  }  
-	  pmem_write(addr, len, data); return; }
+	  pmem_write(addr, len, data); return; 
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
