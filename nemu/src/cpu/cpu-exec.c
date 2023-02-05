435ee682c8a03ffa548f9cc00ca5dc539f6c795b
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-05 15:21:39 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:39 up 19 min,  3 users,  load average: 0.04, 0.08, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -162,6 +162,7 @@ void cpu_exec(uint64_t n) {
 
 //zsl:iringbuf implement*************
   if((nemu_state.state == NEMU_END && nemu_state.halt_ret == 1) || nemu_state.state == NEMU_ABORT ){
+      printf("itrace :\n");
       for(int i=0;i<QUEUE_SIZE;i++){
 	      printf("%s\n",Queue[(QueueOut+i)%QUEUE_SIZE]);
       }
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,6 +18,17 @@
 #include <device/mmio.h>
 #include <isa.h>
 
+
+/* zsl:mtrace Queue structure */
+//#ifdef CONFIG_MTRACE
+//#define MTRACE_QUEUE_ELEMENTS 15
+//#define MTRACE_QUEUE_SIZE (MTRACE_QUEUE_ELEMENTS + 1)
+//char MTQueue[MTRACE_QUEUE_SIZE][128];
+//static int MTQueueIn=0;
+//static int MTQueueOut=0;
+//#endif
+
+
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
@@ -57,7 +68,10 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+	  printf("mtrace:memory read addr:%x length:%d result:%lx\n",addr,len,pmem_read(addr,len));
+	  return pmem_read(addr, len);
+	  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
