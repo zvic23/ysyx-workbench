ce35b2af4ff00784520543a19c4d54c1ec7b0315
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 19:32:59 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:32:59 up 32 min,  5 users,  load average: 0.31, 0.31, 0.29

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -70,7 +70,7 @@ static long load_img() {
 
 //zsl:ftrace 
 struct func{
-char* name;
+char name[20];
 uint64_t addr_start;
 uint64_t addr_end;
 };
@@ -153,7 +153,8 @@ void  __attribute__((optimize("O1")))   ftrace_elf_analysis(char *elf){
   		fseek(fp_ftrace, i*24, SEEK_CUR);
 	  	a= fread(&k, 4, 1, fp_ftrace);
   		assert(a == 1);
-		functab[j].name = &str[k];
+		//functab[j].name = &str[k];
+		strcpy(functab[j].name , &str[k]);
   		fseek(fp_ftrace, 4, SEEK_CUR);
 	  	a= fread(&k, 8, 1, fp_ftrace);
   		assert(a == 1);
