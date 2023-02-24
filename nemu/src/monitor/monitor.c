5ecbe6a04625b587102664c178bb1e4b07e39246
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 10:27:23 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:27:23 up  2:00,  4 users,  load average: 0.25, 0.13, 0.08

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -110,7 +110,7 @@ void  __attribute__((optimize("O1")))   ftrace_elf_analysis(char *elf){
   a= fread(&str, strtab_size, 1, fp_ftrace);
   assert(a == 1);
   int po=1;
-  while(po<=0xc1){
+  while(po<=strtab_size*8){
   	printf("str=%s    ",&str[po]);
 	po+=strlen(&str[po])+1;
   }
