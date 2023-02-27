6c9e9e180e8b249b564da752d6a17ffd1029e7ec
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-27 17:30:15 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:30:15 up  1:32,  7 users,  load average: 0.45, 0.53, 0.38

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -78,7 +78,7 @@ int main() {
   top->clk=1;top->rst=1;step_and_dump_wave();
   top->clk=0;top->rst=0;step_and_dump_wave();
 
-  while(i){
+  while(0){
 
   top->clk = 1;
   //top->Mr_val = pmem_read(top->Mr_addr);
