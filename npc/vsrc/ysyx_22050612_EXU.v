07d4760c8b6afc4496ba652082a8d9cb1caefb6c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-28 08:51:30 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:51:30 up 18 min,  7 users,  load average: 0.16, 0.16, 0.33

--- npc/vsrc/ysyx_22050612_EXU.v
+++ npc/vsrc/ysyx_22050612_EXU.v
@@ -43,14 +43,6 @@ ysyx_22050612_MuxKey #(1, 1, 1) decode0 (wen, opcode, {
 ysyx_22050612_MuxKey #(1, 1, 64) decode1 (wdata, opcode, {
     1'b1, sum0
   });
-//ysyx_22050612_MuxKeyWithDefault #(1, 1, 1) decode0 (wen, opcode, 1'b0, {
-//    1'b1, 1'b1
-//  });
-//ysyx_22050612_MuxKeyWithDefault #(1, 1, 64) decode1 (wdata, opcode, 64'b0,{
-//    1'b1, sum0
-//  });
-
-
 
 
 //  always @(posedge clk) begin
