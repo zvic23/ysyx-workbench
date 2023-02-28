610db689cf35879e0b7ba43a92414882dcc09bff
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-28 17:30:51 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:30:51 up  2:39, 10 users,  load average: 0.25, 0.11, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -94,6 +94,38 @@ void ebreak(int r){
 
 
 
+    char buf[1024] = {0};
+//    printf("请输入数据：");
+//    char *str = fgets(buf, sizeof(buf) - 1, stdin); // fgets() is block
+//    printf("buf : %s\n", buf);
+//    printf("str : %s\n", str);
+//    printf("len : %ld\n", strlen(buf));
+void sdb_mainloop() {
+  for (char *str; (str = fgets(buf, sizeof(buf) - 1, stdin)) != NULL; ) {
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+//    int i;
+//    for (i = 0; i < NR_CMD; i ++) {
+//      if (strcmp(cmd, cmd_table[i].name) == 0) {
+//        if (cmd_table[i].handler(args) < 0) { return; }
+//        break;
+//      }
+//    }
+//
+//    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
   
 
 
@@ -110,12 +142,7 @@ int main() {
 
   while(i){
 
-    char buf[1024] = {0};
-    printf("请输入数据：");
-    char *str = fgets(buf, sizeof(buf) - 1, stdin); // fgets()是阻塞的
-    printf("buf : %s\n", buf);
-    printf("str : %s\n", str);
-    printf("len : %ld\n", strlen(buf));
+
 
 
   }
