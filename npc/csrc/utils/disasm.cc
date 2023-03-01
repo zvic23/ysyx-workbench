be4207971679a9ad80d368cb043cfb6094919ac1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-01 21:01:37 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:37 up  1:58,  8 users,  load average: 0.19, 0.09, 0.09

--- npc/csrc/utils/disasm.cc
+++ npc/csrc/utils/disasm.cc
@@ -92,7 +92,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  //gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
+  gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 
 	//printf("pc=%lx , inst=%x \n",pc ,code);
   std::string s;
