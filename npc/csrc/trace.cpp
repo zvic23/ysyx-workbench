eaa9398d1391e1d78c99971c77c888360ff07035
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-01 20:54:20 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:20 up  1:51,  8 users,  load average: 0.14, 0.11, 0.09

--- npc/csrc/trace.cpp
+++ npc/csrc/trace.cpp
@@ -15,13 +15,14 @@ void itrace(uint64_t pc , uint32_t inst_val){
   char logbuf[128];
   char *p = logbuf;
   p += snprintf(p, sizeof(logbuf), FMT_WORD ":", pc);
+
+printf("p=%s\n",p);
   int ilen = 4;  //s->snpc - s->pc
   int i;
   uint8_t *inst = (uint8_t *)&inst_val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
-printf("p=%s\n",p);
   int ilen_max = 4 ;
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
