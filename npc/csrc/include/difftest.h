9534d3cd0574b0107720eedd9085cc570e57fc44
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 19:54:43 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:54:43 up 34 min,  5 users,  load average: 0.10, 0.17, 0.24

---
+++ npc/csrc/include/difftest.h
@@ -0,0 +1,11 @@
+#ifndef __DIFFTEST_H__
+#define __DIFFTEST_H__
+
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <cinttypes>
+#include <cassert>
+
+void difftest_init(int port) 
+#endif
