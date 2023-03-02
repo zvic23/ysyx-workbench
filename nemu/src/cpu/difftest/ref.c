0da9cbd5dfb15458d80e64695a9519f2a6c5322e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 11:21:36 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:35 up  2:59,  9 users,  load average: 0.09, 0.06, 0.07

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,16 +18,50 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
+
+struct diff_context_t {
+  word_t gpr[32];
+  word_t pc;
+};
+void set_regs(void *diff_context){
+  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
+  for (int i = 0; i < 32; i++) {
+    cpu.gpr[i] = ctx->gpr[i];
+  }
+  cpu.pc = ctx->pc;
+}
+void get_regs(void* diff_context) {
+  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
+  for (int i = 0; i < 32; i++) {
+    ctx->gpr[i] = cpu.gpr[i];
+  }
+  ctx->pc = cpu.pc;
+}
+
+
+
+
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    memcpy(guest_to_host(addr), buf, n);
+  } else {
+    assert(0);
+  }
+  //assert(0);
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    set_regs(dut);
+  } else {
+    get_regs(dut);
+  }
+  //assert(0);
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
+  //assert(0);
 }
 
 void difftest_raise_intr(word_t NO) {
