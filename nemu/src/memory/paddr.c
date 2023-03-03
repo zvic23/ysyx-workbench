405b34d654ddb6e969e822e93faa9cea8f327d9b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 20:00:12 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:00:12 up  1:31, 10 users,  load average: 0.03, 0.02, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -36,7 +36,9 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-uint8_t* guest_to_host(paddr_t paddr) { printf("65:   %d \n",CONFIG_PMEM_MALLOC);printf("66:%hhn \n",pmem); printf("67: %x  %d \n",paddr,CONFIG_MBASE); return pmem + paddr - CONFIG_MBASE; }
+uint8_t* guest_to_host(paddr_t paddr) { 
+	for(int i=0;i<16;i++){printf("pmem[%d]: %x\n",i,pmem[i]);}
+	printf("65:   %d \n",CONFIG_PMEM_MALLOC);printf("66:%hhn \n",pmem); printf("67: %x  %d \n",paddr,CONFIG_MBASE); return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
