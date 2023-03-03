85c994adb77cdb72cd7608b136260a3ab9210bf8
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 16:51:40 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:51:40 up  2:24, 13 users,  load average: 0.34, 0.14, 0.07

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -61,7 +61,7 @@ void difftest_regcpy(void *dut, bool direction) {
   }
   assert(0);
   for (int i = 0; i < 32; i++) {
-    printf("gpr[%d]=%lx  ",i,cpu.gpr[i]);
+    //printf("gpr[%d]=%lx  ",i,cpu.gpr[i]);
   }
   //assert(0);
 }
