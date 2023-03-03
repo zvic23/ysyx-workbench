7f11fad30935c72437f837a9291c9a2adce39342
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 19:10:53 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:53 up 42 min,  8 users,  load average: 0.05, 0.07, 0.10

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -49,7 +49,7 @@ void get_regs(void* diff_context) {
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-	  printf("111:%x\n",addr);
+	  printf("111:%x   buf:%x\n",addr,buf);
     memcpy(guest_to_host(addr), buf, n);
   } else {
     assert(0);
