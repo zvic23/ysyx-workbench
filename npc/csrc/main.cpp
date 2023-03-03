c986eb342667dbbd533d46aa8c641529ae64b6ea
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 17:21:34 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:34 up  2:54, 13 users,  load average: 0.21, 0.18, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -152,12 +152,12 @@ init_difftest(a,b);
   top->clk=0;top->rst=1;step_and_dump_wave();
   top->clk=1;top->rst=1;step_and_dump_wave();
   top->clk=0;top->rst=0;step_and_dump_wave();    //init the npc
-
-  sdb_mainloop();
   int i;
   for (i = 0; i < 32; i++) {
     printf("gpr[%d] = 0x%lx\n", i, cpu_gpr[i]);
   }
+  sdb_mainloop();
+
   while(0){
 
 
