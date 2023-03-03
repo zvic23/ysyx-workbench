6adbb274daab99bd4665269e6c6d9945f39c6b75
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 11:08:19 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:19 up  2:38,  8 users,  load average: 0.31, 0.25, 0.20

--- nemu/include/difftest.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DIFFTEST_H__
-#define __DIFFTEST_H__
-
-
-
-void difftest_init(int port); 
-#endif
---
+++ nemu/include/difftesting.h
@@ -0,0 +1,7 @@
+#ifndef __DIFFTESTING_H__
+#define __DIFFTESTING_H__
+
+
+
+void difftest_init(int port); 
+#endif
