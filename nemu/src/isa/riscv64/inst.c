901a2542b6555383c2e274adccb7c44bffb4202d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-04 10:13:32 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:13:32 up 34 min, 10 users,  load average: 0.24, 0.14, 0.10

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -212,7 +212,7 @@ static int decode_exec(Decode *s) {
 }
 
 int isa_exec_once(Decode *s) {
-	printf("11\n");
+	printf("snpc:%lx\n",s->snpc);
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
 }
