0afcd72532f601404f5fdc12327ef59c4036553d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-04 09:57:15 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:57:15 up 18 min,  5 users,  load average: 0.01, 0.07, 0.08

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -38,10 +38,10 @@ void get_regs(void* diff_context) {
   struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
   for (int i = 0; i < 32; i++) {
     ctx->gpr[i] = cpu.gpr[i];
-    //printf("nemu.gpr=%lx,   npc.gpr=%lx\n",cpu.gpr[i],ctx->gpr[i]);
+    printf("nemu.gpr=%lx,   npc.gpr=%lx\n",cpu.gpr[i],ctx->gpr[i]);
   }
   ctx->pc = cpu.pc;
-    //printf("nemu.pc=%lx,   npc.pc=%lx\n",cpu.pc,ctx->pc);
+    printf("nemu.pc=%lx,   npc.pc=%lx\n",cpu.pc,ctx->pc);
 }
 
 
