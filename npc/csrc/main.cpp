687e5728e35c73b75f6a5c787700654550d1b7ad
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 11:08:06 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:06 up  2:37,  8 users,  load average: 0.14, 0.06, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -57,9 +57,9 @@ uint32_t pmem_read(uint64_t addr){
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   if(raddr>=0x80000000){
-  	long long raddr_set = raddr & ~0x7ull;
+  	long long raddr_set = raddr & ~0x7ull -0x80000000;
 	long long data;
-	memset(&data, pmem[raddr_set-0x80000000], 8);
+	memset(&data, pmem[raddr_set], 8);
   	rdata = &data;
 
 #ifdef CONFIG_MTRACE
