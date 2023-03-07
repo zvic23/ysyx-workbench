6320e2752dfc9205df5f6fff6b6e73a8f62154bd
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-07 14:54:04 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:04 up 27 min,  6 users,  load average: 0.13, 0.12, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -139,6 +139,7 @@ void ebreak(int r){
 
 uint32_t inst;
 void read_inst(int npc_inst){
+	printf("inst=%lx\n",npc_inst);
 	inst = npc_inst;
 }
 
@@ -146,7 +147,7 @@ void read_inst(int npc_inst){
 
 void one_cycle(){
 #ifdef CONFIG_ITRACE
-  itrace(top->pc, inst);
+  //itrace(top->pc, inst);
 #endif
   step_and_dump_wave();
 
