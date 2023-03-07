f06ccb7d6cf3cc0e09609518ef600844aea6741f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-07 15:31:17 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:31:17 up  1:05,  7 users,  load average: 0.10, 0.08, 0.04

--- npc/csrc/infrastructure/dut.c
+++ npc/csrc/infrastructure/dut.c
@@ -6,8 +6,9 @@
 #include <cstdio>
 
 #include "../include/difftesting.h"
+#include "../include/generated/autoconf.h"
 
-
+#ifdef CONFIG_DIFFTEST
 void (*ref_difftest_memcpy)(uint64_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -24,7 +25,6 @@ static int skip_dut_nr_inst = 0;
 
 
 
-#ifdef CONFIG_DIFFTEST
 void init_difftest(long img_size, int port) {
   char ref_so_file[100]="/home/zsl/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so";
 
