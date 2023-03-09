75ed869f93adee3ba611560ef8ed22484d211b48
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 09:10:41 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:10:41 up 42 min,  6 users,  load average: 0.30, 0.13, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -73,7 +73,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
 #ifdef CONFIG_MTRACE		
 	memcpy(&rdata_printf, &pmem[raddr_set-0x80000000], 8);
 	if(raddr >= CONFIG_MTRACE_START && raddr <= CONFIG_MTRACE_END){
-	  	printf("mtrace:memory read    addr:0x%llx(0x%llx)   data:0x%lld \n",\
+	  	printf("mtrace:memory read    addr:0x%llx(0x%llx)   data:0x%llu \n",\
 		raddr,raddr_set,rdata_printf);
 	} 
 #endif
