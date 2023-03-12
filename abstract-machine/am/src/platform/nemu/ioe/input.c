df2cb34071ff319b6397972c3b24c071b8cc4b0d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-12 14:58:36 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:58:36 up 50 min,  8 users,  load average: 0.25, 0.22, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
@@ -8,7 +7,6 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint64_t keyvalue = inw(KBD_ADDR);
   kbd->keydown = keyvalue & KEYDOWN_MASK;
   kbd->keycode = keyvalue & ~KEYDOWN_MASK;
-  printf("111:%d\n",AM_KEY_NONE);
   //kbd->keydown = 0;
   //kbd->keycode = AM_KEY_NONE;
 }
--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 void __am_timer_init() {
 }
@@ -9,7 +8,6 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint64_t us_low = inl(RTC_ADDR);
   uint64_t us_high= inl(RTC_ADDR+0x4);
   uint64_t us_whole= us_high+ us_low;
-  //printf("111:%d  %d\n",us_low,us_whole);
   uptime->us = us_whole;
   //uptime->us = 0;
 }
