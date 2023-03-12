20c91c61d57393be23fd0dbfe4a8513b572b7f8b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-12 15:40:00 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:40:00 up  1:31,  9 users,  load average: 0.06, 0.34, 0.28

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,10 +29,16 @@ int atoi(const char* nptr) {
   return x;
 }
 
+  static char *addr;
+  static int init_malloc=0;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if(init_malloc == 0){
+  addr = (void *)ROUNDUP(heap.start, 8);
+  init_malloc = 1;
+  }
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
