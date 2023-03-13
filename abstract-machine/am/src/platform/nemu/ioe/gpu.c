f52c7077d170b3ade48cb7c31b67608463feda6b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 16:16:53 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:52 up  2:30,  9 users,  load average: 0.34, 0.20, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,7 +6,7 @@
 
 void __am_gpu_init() {
   int i;
-  int w = (inw(VGACTL_ADDR)&0xffff)>>4;  // TODO: get the correct width
+  int w = (inw(VGACTL_ADDR)&0xffff0000)>>4;  // TODO: get the correct width
   int h = inw(VGACTL_ADDR)&0xffff;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   printf("width=%d   height=%d\n",w,h);
