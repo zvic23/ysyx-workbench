eedd2f1c980ff4690a2354e9e4c3a93bf7498886
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 15:52:29 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:52:29 up  2:06,  8 users,  load average: 0.04, 0.06, 0.05

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,7 +73,11 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-
+  //if(io_read(AM_GPU_STATUS).ready != 0){
+  //        update_screen();
+  //        io_write(AM_GPU_STATUS,0);
+  //}
+  printf("port:%d\n",vgactl_port_base[0]);
 }
 
 void init_vga() {
