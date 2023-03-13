b41625936aa15d504b9a329fcf759f181112c4c6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 22:15:07 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:15:06 up 29 min,  7 users,  load average: 0.14, 0.27, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,18 +5,18 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = inl(VGACTL_ADDR)>>16;  // TODO: get the correct width
-  int h = inw(VGACTL_ADDR);  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+//  int i;
+//  int w = inl(VGACTL_ADDR)>>16;  // TODO: get the correct width
+//  int h = inw(VGACTL_ADDR);  // TODO: get the correct height
+//  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//  for (i = 0; i < w * h; i ++) fb[i] = i;
+//  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inl(VGACTL_ADDR)>>16, .height = inw(VGACTL_ADDR),
+    //.width = inl(VGACTL_ADDR)>>16, .height = inw(VGACTL_ADDR),
     //.width = 0, .height = 0,
     .vmemsz = 0
   };
@@ -31,7 +31,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
   for(int i=ctl->y;i<ctl->y+ctl->h;i++){
 	  for(int j=ctl->x;j<ctl->x+ctl->w;j++){
-		  //outl(FB_ADDR+i*400*4+j*4  , *(uint32_t*)ctl->pixels );
+		  outl(FB_ADDR+i*400*4+j*4  , *(uint32_t*)ctl->pixels );
 	  }
   }
 }
