2e4da218b8b49fcf44c4abc2d5cbdd5f698d9e54
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 10:37:26 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:37:26 up 37 min, 10 users,  load average: 0.54, 0.52, 0.40

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -10,6 +10,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   printf("keyvalue:%d\n",keyvalue);
   kbd->keydown = keyvalue & KEYDOWN_MASK;
   kbd->keycode = keyvalue & ~KEYDOWN_MASK;
+  if(keyvalue == 0)kbd->keycode = AM_KEY_NONE;
   //kbd->keydown = 0;
   //kbd->keycode = AM_KEY_NONE;
 }
