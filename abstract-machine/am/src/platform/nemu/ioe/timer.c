f71e9fbb309ed13b882e1d49ee8e4e27d0e450e1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 11:02:59 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:59 up  1:02, 10 users,  load average: 0.75, 0.70, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,11 +5,11 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  //uint64_t us_low = inl(RTC_ADDR);
-  //uint64_t us_high= inl(RTC_ADDR+0x4);
-  //uint64_t us_whole= us_high + us_low;
-  //uptime->us = us_whole;
-  uptime->us = ((uint64_t)inl(RTC_ADDR+4)<<32)+inl(RTC_ADDR);
+  uint64_t us_low = inl(RTC_ADDR);
+  uint64_t us_high= inl(RTC_ADDR+0x4);
+  uint64_t us_whole= us_high + us_low;
+  uptime->us = us_whole;
+  //uptime->us = ((uint64_t)inl(RTC_ADDR+4)<<32)+inl(RTC_ADDR);
   //uptime->us = 0;
 }
 
