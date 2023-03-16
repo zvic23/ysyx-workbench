73d5a8b8e07ddc4b0823ed88beb6f0d4b81593a7
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 10:59:21 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:59:21 up  1:08, 10 users,  load average: 0.48, 0.32, 0.25

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -5,7 +5,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t *RTC_ADDR = (uint32_t*)0xa0000048;
-  uint64_t us_low = *RTC_ADDR;
+  uint64_t us_low = RTC_ADDR[0];
   uint64_t us_high= RTC_ADDR[1];
   uint64_t us_whole= (us_high<<32) + us_low;
   uptime->us = us_whole;
