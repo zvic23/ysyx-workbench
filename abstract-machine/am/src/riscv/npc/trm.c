a7f7a65decfc7da669409b8944ba021b766ba4d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 10:34:07 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:34:07 up 43 min, 10 users,  load average: 0.38, 0.27, 0.21

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -15,7 +15,9 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  outb(0xa00003f8, ch);
+  char *SERIAL_PORT = (char*)0x800003f8;
+  *SERIAL_PORT = ch;
+  //outb(0xa00003f8, ch);
 }
 
 void halt(int code) {
