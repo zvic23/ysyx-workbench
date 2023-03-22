5fb0cf32698b13fd5db1686e9b02b6f3822f7dbb
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 21:20:21 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:20:21 up  1:37, 12 users,  load average: 0.25, 0.16, 0.15

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -19,3 +19,39 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->month  = 0;
   rtc->year   = 1900;
 }
+
+
+void __am_gpu_init() {
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = 400, .height = 300,
+    .vmemsz = 0
+  };
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  //printf("x=%d   y=%d   w=%d   h=%d   pixel=%d   sync=%d\n",ctl->x,ctl->y,ctl->w,ctl->h,*(uint32_t*)ctl->pixels,ctl->sync);
+  if (ctl->sync) {
+    //outl(SYNC_ADDR, 1);
+    uint32_t *SYNC_ADDR = (uint32_t*)0xa0000104;
+    SYNC_ADDR[0] = 1;
+  }
+  uint32_t *pixel = (uint32_t*)ctl->pixels;
+  uint32_t *FB_ADDR = (uint32_t*)0xa1000000;
+  int p=0;
+  for(int i=ctl->y;i<ctl->y+ctl->h;i++){
+	  for(int j=ctl->x;j<ctl->x+ctl->w;j++){
+		  //outl(FB_ADDR+i*400*4+j*4  , pixel[p] );
+		  FB_ADDR[i*400+j]=pixel[p];
+		  p++;
+	  }
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
