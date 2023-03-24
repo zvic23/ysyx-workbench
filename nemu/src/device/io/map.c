f4c3dbc4468f5020e090d6654fa4b660fb8ae62e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 10:17:50 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:17:50 up  1:08, 10 users,  load average: 0.54, 0.71, 0.68

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
 
-  printf("%s write\n",map->name);
+  printf("%s read\n",map->name);
   return ret;
 }
 
