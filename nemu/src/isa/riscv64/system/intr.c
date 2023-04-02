1b1cd82d98ce9b2bd0c045218096a15407d9128f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 16:39:26 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:39:26 up  2:06, 10 users,  load average: 0.23, 0.14, 0.10

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -21,9 +21,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  mepc=cpu.pc;
-  mcause=NO;
-  cpu.pc=epc;
+
   return 0;
 }
 
