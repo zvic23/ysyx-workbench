b75ae6f894e14d6291afdbdf30d42d2b66e9802a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 10:27:21 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:27:21 up  1:25,  9 users,  load average: 0.05, 0.10, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,30 +16,30 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&phoff, 32, 8);
   printf("phoff=%ld\n",phoff);
 
-  uint64_t phentsize0;
-  ramdisk_read(&phentsize0, 54, 2);
-  //printf("phentsize=%ld\n",phentsize0);
+//  uint64_t phentsize0;
+//  ramdisk_read(&phentsize0, 54, 2);
+//  printf("phentsize=%ld\n",phentsize0);
 
   uint64_t phentsize;
   ramdisk_read(&phentsize, 54, 2);
-  //printf("phentsize=%ld\n",phentsize);
+  printf("phentsize=%ld\n",phentsize);
 
   uint64_t phnum;
   ramdisk_read(&phnum, 56, 2);
   printf("phnum=%ld\n",phnum);
 
-  uint64_t p_type0;
-  ramdisk_read(&p_type0, phoff, 4);
-  printf("type=%ld\n",p_type0);
-  uint64_t p_type1;
-  ramdisk_read(&p_type1, phoff+phentsize, 4);
-  printf("type=%ld\n",p_type1);
-  uint64_t p_type2;
-  ramdisk_read(&p_type2, phoff+phentsize*2, 4);
-  printf("type=%ld\n",p_type2);
-  uint64_t p_type3;
-  ramdisk_read(&p_type3, phoff+phentsize*3, 4);
-  printf("type=%ld\n",p_type3);
+//  uint64_t p_type0;
+//  ramdisk_read(&p_type0, phoff, 4);
+//  printf("type=%ld\n",p_type0);
+//  uint64_t p_type1;
+//  ramdisk_read(&p_type1, phoff+phentsize, 4);
+//  printf("type=%ld\n",p_type1);
+//  uint64_t p_type2;
+//  ramdisk_read(&p_type2, phoff+phentsize*2, 4);
+//  printf("type=%ld\n",p_type2);
+//  uint64_t p_type3;
+//  ramdisk_read(&p_type3, phoff+phentsize*3, 4);
+//  printf("type=%ld\n",p_type3);
 //  uint64_t p_type;
 //  for(int i=0; i<phnum; i++){
 //	  ramdisk_read(&p_type, phoff+phentsize*i, 4);
