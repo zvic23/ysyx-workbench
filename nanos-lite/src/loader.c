cb8c265d27372c85cc2dcf119aa14a41064b0965
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 19:49:00 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:00 up  1:22, 12 users,  load average: 0.10, 0.13, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,7 +24,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&machine, 18, 2);
   printf("machine=%d\n",machine);
   uint16_t machine1 = 2;
-  assert(machine1= EXPECT_TYPE);
+  assert(machine1==EXPECT_TYPE);
 
   uint8_t ident[16];
   ramdisk_read(ident, 0, 16);
