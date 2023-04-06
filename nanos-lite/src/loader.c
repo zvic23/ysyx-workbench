8e80d4aa1c34ca1821f145cf4f172118c91f4a86
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 10:26:13 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:26:13 up  1:23,  9 users,  load average: 0.15, 0.13, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("phnum=%ld\n",phnum);
 
   uint64_t p_type0;
-  ramdisk_read(&p_type0, 56, 2);
+  ramdisk_read(&p_type0, phoff, 4);
   printf("type=%ld\n",p_type0);
 //  uint64_t p_type1;
 //  ramdisk_read(&p_type1, phoff+phentsize, 4);
