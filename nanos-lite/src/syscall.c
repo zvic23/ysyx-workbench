4eeaa2f653b2d3b5576bbd27b3e8b2f577cb68bf
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 09:40:19 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:40:19 up  1:03, 11 users,  load average: 1.08, 0.45, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,10 @@
 #include <common.h>
 #include "syscall.h"
+
+void do_write(uint64_t a,uint64_t b,uint64_t c) {
+  printf("!!\n");
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -11,6 +16,9 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: halt(a[1]);break;
     case 1: yield();break;
+    case 4: do_write(a[1],a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+//zsl:the next is mine
+
