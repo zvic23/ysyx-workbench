d3a93ced346bc78110ec1d3ea1dd78ccce0dbb7e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 09:12:46 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:12:46 up 36 min, 10 users,  load average: 0.27, 0.10, 0.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,6 +4,9 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  printf("strace: syscall[%d]: %x %x %x\n",a[0],a[1],a[2],a[3]);
 
   switch (a[0]) {
     case 0: halt(a[1]);break;
