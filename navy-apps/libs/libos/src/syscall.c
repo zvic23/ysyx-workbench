2988168b25040cbdca2c3815785032a36db84a60
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 11:12:44 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:12:44 up  2:35, 11 users,  load average: 0.39, 0.20, 0.14

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -73,7 +73,7 @@ void *_sbrk(intptr_t increment) {
   static intptr_t program_break = (uintptr_t)&_end;
   intptr_t program_break_old;
   char aa[10];
-  sprintf(aa, "%lx", program_break);
+  sprintf(aa, "%lx", increment);
   _write(1,aa,10);
   if((_syscall_(SYS_brk, increment, 0, 0))==0){
   	  program_break_old = program_break;
