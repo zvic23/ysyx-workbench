6341c44a0cef0ac7c31bc904b76f5dcdb55be4ff
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 19:35:30 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:30 up  1:36,  8 users,  load average: 0.12, 0.14, 0.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,19 +1,20 @@
 #include <common.h>
 #include "syscall.h"
 
-void do_write(uint64_t a,uint64_t b,uint64_t c) {
-  uint8_t *buf=(uint8_t*)b;
-  if(a==1 || a==2){
-	  for(int i=0;i<c;i++){
+void do_write(Context *c) {
+  uint8_t *buf=(uint8_t*)c->GPR3;
+  if(c->GPR2==1 || c->GPR2==2){
+	  for(int i=0;i<c->GPR4;i++){
 		  putch(buf[i]);
 	  }
   }
-  asm volatile("mv a0, %0" : :"r"(c));
+  c->GPRx = c->GPR4;
+  //asm volatile("mv a0, %0" : :"r"(c));
 }
 
 void do_brk(Context *c) {
-  //c->GPRx = 0;
-  asm volatile("li a0, 0");
+  c->GPRx = 0;
+  //asm volatile("li a0, 0");
 }
 
 void do_syscall(Context *c) {
@@ -27,7 +28,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: halt(a[1]);break;
     case 1: yield();break;
-    case 4: do_write(a[1],a[2],a[3]);break;
+    case 4: do_write(c);break;
     case 9: do_brk(c);break;//printf("RET:%x\n",c->GPR2);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
