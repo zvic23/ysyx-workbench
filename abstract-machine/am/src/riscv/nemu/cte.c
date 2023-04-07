a013830045b760ff7091890e2f52f514bf54b76e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 08:50:53 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:50:53 up 14 min,  6 users,  load average: 0.42, 0.42, 0.47

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,7 +37,8 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a0, 0");
+  //asm volatile("li a7, -1; ecall");
 }
 
 bool ienabled() {
