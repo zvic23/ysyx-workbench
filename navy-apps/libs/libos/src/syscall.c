d59e8d114c3e0037744a0226ddc1de8788f5fa98
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 20:20:33 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:20:33 up 43 min,  8 users,  load average: 0.04, 0.17, 0.24

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -99,14 +99,15 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_close, fd, (uintptr_t)buf, count);
+  return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
   //_exit(SYS_read);
   //return 0;
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
+  return _syscall_(SYS_close, fd, 0, 0);
+  //_exit(SYS_close);
+  //return 0;
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
