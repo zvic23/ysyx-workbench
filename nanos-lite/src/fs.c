21d4a3a47daa05c5234045ec30cff975c9887b51
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 20:59:05 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:05 up  1:22,  9 users,  load average: 0.37, 0.23, 0.21

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -46,6 +46,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 static uint64_t position[1000];
+//enum {SEEK_SET=1,SEEK_CUR,SEEK_END};
 size_t fs_lseek(int fd, size_t offset, int whence){
 	size_t f_size = file_table[fd].size;
 	//size_t f_offset = file_table[fd].disk_offset;
@@ -57,21 +58,12 @@ size_t fs_lseek(int fd, size_t offset, int whence){
 		position[fd] = f_size + offset;
 		//position[fd] = f_size + f_offset + offset;
 	}else assert(0);
-	if(position[fd] > f_size){
-		printf("file system:out of bound\n");
-		assert(0);
-	}
 	return position[fd];
 }
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t fs_read(int fd, void *buf, size_t len){
 	size_t f_offset = file_table[fd].disk_offset;
-	size_t f_size = file_table[fd].size;
-	if(position[fd] + len >= f_size){
-		printf("file system:out of bound\n");
-		assert(0);
-	}
 	ramdisk_read(buf, f_offset+position[fd], len);
 	position[fd] += len;
 	return len;
@@ -80,11 +72,6 @@ size_t fs_read(int fd, void *buf, size_t len){
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len){
 	size_t f_offset = file_table[fd].disk_offset;
-	size_t f_size = file_table[fd].size;
-	if(position[fd] + len >= f_size){
-		printf("file system:out of bound\n");
-		assert(0);
-	}
 	ramdisk_write(buf, f_offset+position[fd], len);
 	position[fd] += len;
 	return len;
