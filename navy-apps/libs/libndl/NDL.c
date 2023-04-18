10fbbe725df0a5ab2bf7e4a4d7b0efb5903b9c8f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-18 20:23:59 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:59 up  1:11, 10 users,  load average: 0.21, 0.24, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -20,6 +20,7 @@ int NDL_PollEvent(char *buf, int len) {
   FILE *fp = fopen("/dev/events", "r+");
   int succ = fscanf(fp, buf, len);
   fclose(fp);
+  printf("succ = %d\n",succ);
   if(succ) return 0;
   else return 0;
 }
