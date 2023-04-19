9004d43d594b89052c9d94d2b64978fd546012cb
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-19 16:26:48 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:26:48 up  1:06,  9 users,  load average: 0.71, 0.53, 0.32

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,7 +52,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   printf("x=%d,y=%d\n",x,y);
   uint32_t bufff[200];
   memset(bufff,0xff,800);
-  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)bufff, len, 0, false);
+  io_write(AM_GPU_FBDRAW, x, y, (uint32_t*)bufff, len, 1, false);
   io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
 
 
