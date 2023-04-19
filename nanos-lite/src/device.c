7e0ed2962383bf981a3f79a36fbc7c0c1ae620b0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-19 10:35:06 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:35:06 up 47 min,  7 users,  load average: 0.31, 0.32, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,11 +35,12 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int h = io_read(AM_GPU_CONFIG).height;  
   printf("in:  w=%d  h=%d\n",w,h);
   uint64_t mix = ((uint64_t)w<<32)+ h;
+  printf("mix:%ld\n",mix);
   //char str_buf[100];
   //sprintf(str_buf,"WIDTH:%d\nHEIGHT:%d",w,h);
 
   //snprintf(buf, len, "%ld",mix);
-  *(uint64_t*)buf = mix>>32;
+  *(uint64_t*)buf = mix;
   printf("buf:%ld\n",buf);
   //snprintf(buf, len, "%s",str_buf);
   return 0;
