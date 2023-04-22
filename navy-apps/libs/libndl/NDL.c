5f767b8dedadf2a8c2ab4ef3def82328e1458e3b
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 20:06:53 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:53 up  1:05, 10 users,  load average: 0.60, 0.51, 0.39

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -89,25 +89,17 @@ int NDL_Init(uint32_t flags) {
 
 
 
-//  char buf0[20];
-//  char buf1[20];
-//  FILE *fp = fopen("/proc/dispinfo", "r+");
-//  printf("111\n");
-//  int succ = fscanf(fp, "%s\n%s", buf0,buf1);
+
   char buf[40];
   int fp = open("/proc/dispinfo", "r+");
   printf("111\n");
   int succ = read(fp, buf, 100);
-
   printf("111\n");
-  //printf("succ:%d\n",succ);
-  //printf("str:%s\n",buf);
-  //printf("str:%s\n",buf1);
   int w,h;
   sscanf(buf,"WIDTH:%d\nHEIGHT:%d",&w,&h);
 
   printf("211\n");
-  //printf("wh:%d  %d\n",w,h);
+  printf("wh:%d  %d\n",w,h);
   close(fp);
 
   printf("311\n");
