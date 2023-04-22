c465f482736139f441aba2bc6a0b87c06ab2bc38
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 19:24:58 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:24:58 up 23 min, 11 users,  load average: 0.39, 0.31, 0.25

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,9 +1,5 @@
 #include <common.h>
 
-#include <am.h>
-#include <klib.h>
-#include <klib-macros.h>
-
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
 # define MULTIPROGRAM_YIELD() yield()
 #else
