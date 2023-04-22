981da3a415cd33deaabfae2c7fc5e8574a0262a2
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 19:30:05 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:05 up 28 min, 11 users,  load average: 0.11, 0.21, 0.22

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
   else{
-	  //snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode] );
+	  snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode] );
   	  return len;
   }
 }
