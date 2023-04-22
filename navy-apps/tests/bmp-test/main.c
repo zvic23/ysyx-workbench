8e9435dce4988ac2beed97587024cd5f77c06673
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 20:10:09 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:10:09 up  1:08, 10 users,  load average: 0.52, 0.62, 0.46

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -6,8 +6,10 @@
 
 int main() {
   NDL_Init(0);
+  printf("in\n");
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("in1\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
