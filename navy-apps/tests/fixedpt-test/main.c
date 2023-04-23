3a6b390981d3013976987988d4a0c0fea01dc144
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 14:45:29 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:29 up 19 min,  5 users,  load average: 0.10, 0.09, 0.10

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -26,8 +26,8 @@ if (b > fixedpt_rconst(7.9)) {
 
   printf("c=%d\n",c);
 
-int f=3.5;
-fixedpt g = fixedpt_fromint(5);
+int f=5;
+fixedpt g = fixedpt_rconst(3.5);
 int h = 0;
   h = fixedpt_toint(fixedpt_divi(fixedpt_muli(g + FIXEDPT_ONE, f), 2));
 
