2198420413140578bb48b98525763551920c12cc
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 15:50:30 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:50:30 up  1:24,  5 users,  load average: 0.14, 0.12, 0.12

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -173,7 +173,7 @@ static inline fixedpt fixedpt_floor(fixedpt A) {
 	if((fixedptu)A_whole>>(FIXEDPT_BITS-1) == 0)return A_whole;
 	fixedpt A_abs = fixedpt_abs(A);
 	if(fixedpt_fracpart(A_abs)==0) return A;
-	else return A;
+	else return A&~FIXEDPT_FMASK;
 	//else return -((A_abs&~FIXEDPT_FMASK)+1);
 
 
