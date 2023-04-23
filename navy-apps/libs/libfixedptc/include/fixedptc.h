21f570956304c01ac5c01f4cd5ca76c0f30bbcb3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 11:04:42 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:04:42 up  2:42,  8 users,  load average: 0.35, 0.33, 0.25

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -144,7 +144,7 @@ static inline fixedpt fixedpt_divi(fixedpt A, int B) {
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
 	fixedptd C = A * B >> FIXEDPT_FBITS;
-	printf("A=%x   B=%x  C=%lx(%ld)  width=%d\n",A,B,C,fixedpt_toint(C),FIXEDPT_FBITS);
+	printf("mul:  A=%x   B=%x  C=%lx(%ld)  width=%d\n",A,B,C,fixedpt_toint(C),FIXEDPT_FBITS);
 	return (fixedpt)C;
 	//return 0;
 }
@@ -152,8 +152,9 @@ static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	fixedpt C = A * B * (2^FIXEDPT_FBITS);
-	return C;
+	fixedptd C = A * B >> FIXEDPT_FBITS;
+	printf("div:  A=%x   B=%x  C=%lx(%ld)  width=%d\n",A,B,C,fixedpt_toint(C),FIXEDPT_FBITS);
+	return (fixedpt)C;
 	//return 0;
 }
 
