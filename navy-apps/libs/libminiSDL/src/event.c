9e80c2155cd3cafe78f754de7239508b7f3446c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 08:38:05 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:38:05 up 16 min,  3 users,  load average: 0.03, 0.11, 0.10

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -9,21 +9,31 @@ static const char *keyname[] = {
 };
 
 int SDL_PushEvent(SDL_Event *ev) {
+	printf("sdl not implement!\n");
+	assert(0);
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+	printf("sdl not implement!\n");
+	assert(0);
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+	printf("sdl not implement!\n");
+	assert(0);
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+	printf("sdl not implement!\n");
+	assert(0);
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
+	printf("sdl not implement!\n");
+	assert(0);
   return NULL;
 }
--- navy-apps/libs/libminiSDL/src/file.c
+++ navy-apps/libs/libminiSDL/src/file.c
@@ -1,9 +1,13 @@
 #include <sdl-file.h>
 
 SDL_RWops* SDL_RWFromFile(const char *filename, const char *mode) {
+	printf("sdl not implement!\n");
+	assert(0);
   return NULL;
 }
 
 SDL_RWops* SDL_RWFromMem(void *mem, int size) {
+	printf("sdl not implement!\n");
+	assert(0);
   return NULL;
 }
