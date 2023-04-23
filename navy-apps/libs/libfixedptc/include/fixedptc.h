aad33001802ce59660dfc7452c5351431e3070e0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-23 11:17:21 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:17:21 up  2:55,  8 users,  load average: 0.54, 0.37, 0.30

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -143,8 +143,8 @@ static inline fixedpt fixedpt_divi(fixedpt A, int B) {
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	int32_t tt=2^1;
-	int32_t tt0=2;
+	int32_t tt=2^2;
+	int32_t tt0=4;
 	printf("%d  %d\n",tt,tt0);
 	fixedptd C = A * B /tt;
 	//fixedptd C = A * B >> FIXEDPT_FBITS;
