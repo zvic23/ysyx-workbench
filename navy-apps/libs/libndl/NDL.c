5d1b6ef87f58d04739c3fba9964b41d6b0510938
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-24 16:21:09 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:09 up  1:13, 11 users,  load average: 0.81, 0.49, 0.31

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -71,16 +71,16 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int x_mid=(screen_w-canvas_w)/2;
   int y_mid=(screen_h-canvas_h)/2;
   int fp = open("/dev/fb", "r+");
-//  for(int i=0;i<h;i++){
-//	  //printf("i=%d\n",i);
-//          //memcpy(pixel_buf+x_mid+x+screen_w*(i+y_mid),&pixels[w*i],w*4);
-//          //memcpy(pixel_buf+screen_w*i,&pixels[w*i],w*4);
-//	  lseek(fp,(x_mid+x+screen_w*(i+y_mid))*4,SEEK_SET);
-//	  write(fp,&pixels[w*i],w*4);
-//  }
+  for(int i=0;i<h;i++){
+	  //printf("i=%d\n",i);
+          //memcpy(pixel_buf+x_mid+x+screen_w*(i+y_mid),&pixels[w*i],w*4);
+          //memcpy(pixel_buf+screen_w*i,&pixels[w*i],w*4);
+	  lseek(fp,(x_mid+x+screen_w*(i+y_mid))*4,SEEK_SET);
+	  write(fp,&pixels[w*i],w*4);
+  }
 	  //memcpy(pixel_buf,pixels,screen_w*screen_h*4);
   //write(fp,pixel_buf,screen_w*screen_h*4);
-  write(fp,pixels,screen_w*screen_h*4);
+  //write(fp,pixels,screen_w*screen_h*4);
 
   close(fp);
 
