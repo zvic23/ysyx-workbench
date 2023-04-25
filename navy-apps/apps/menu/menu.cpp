c9eb6a39d6735363fbfec91bf19c5a180b2137a8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 19:07:35 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:07:35 up 9 min,  5 users,  load average: 0.48, 0.54, 0.35

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -143,8 +143,8 @@ static void display_menu(int n) {
   clear_display();
   printf("111\n");
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("121\n");
+  SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
   int i;
