adfa86a326df7a0d735675adda5e849c5856456a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 19:13:46 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:46 up 15 min,  6 users,  load average: 0.71, 0.58, 0.44

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -32,8 +32,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	uint32_t *src_p = (uint32_t*)src->pixels;
 	for(int i=0;i<srcrect_h;i++){
 		for(int j=0;j<srcrect_w;j++){
-			dst_p[(dstrect_y+i)*dst_w+dstrect_x+j]=src_p[(srcrect_y+i)*src_w+srcrect_x+j];
-			//src_buf[pst++]=src_p[(srcrect_y+i)*src_w+srcrect_x+j];
+			src_buf[pst++]=src_p[(srcrect_y+i)*src_w+srcrect_x+j];
 		}
 	}
 	pst = 0;
