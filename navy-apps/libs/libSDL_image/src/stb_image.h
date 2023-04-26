df9a49f9d0242ecd7b2c1319f63cabac7e48c9ce
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 19:07:05 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:07:05 up  4:34, 14 users,  load average: 0.63, 0.45, 0.22

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -3942,6 +3942,7 @@ static int stbi__png_info(stbi__context *s, int *x, int *y, int *comp)
 {
    stbi__png p;
    p.s = s;
+   printf("gggg\n");
    return stbi__png_info_raw(&p, x, y, comp);
 }
 
