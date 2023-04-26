b79229819899b1c0697431a97311ddc6a0423242
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 19:04:30 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:04:30 up  4:31, 14 users,  load average: 0.61, 0.35, 0.15

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -5344,29 +5344,33 @@ static int      stbi__pnm_info(stbi__context *s, int *x, int *y, int *comp)
 
 static int stbi__info_main(stbi__context *s, int *x, int *y, int *comp)
 {
-printf("sss\n");
    #ifndef STBI_NO_JPEG
    if (stbi__jpeg_info(s, x, y, comp)) return 1;
    #endif
 
+printf("sss\n");
    #ifndef STBI_NO_PNG
    if (stbi__png_info(s, x, y, comp))  return 1;
    #endif
 
+printf("sss\n");
    #ifndef STBI_NO_GIF
    if (stbi__gif_info(s, x, y, comp))  return 1;
    #endif
 
+printf("sss\n");
    #ifndef STBI_NO_BMP
    if (stbi__bmp_info(s, x, y, comp))  return 1;
    #endif
 
 
+printf("sss\n");
    #ifndef STBI_NO_PNM
    if (stbi__pnm_info(s, x, y, comp))  return 1;
    #endif
 
 
+printf("sss\n");
    // test tga last because it's a crappy test!
    #ifndef STBI_NO_TGA
    if (stbi__tga_info(s, x, y, comp))
