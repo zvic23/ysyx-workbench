5041763e5c7bdea0ffcf58d6785324e6349d93b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-27 10:43:58 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:58 up 32 min,  8 users,  load average: 0.49, 0.45, 0.32

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -58,6 +58,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
+	while(1);
 	int re = _syscall_(SYS_open, path, 0, 0);
 	char buf0[20];
 	  sprintf(buf0 , "%x\n", re);
