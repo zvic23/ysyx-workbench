af3c1b4e1db776640eb75c953e9d3c1dcba7a42e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-27 10:25:50 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:25:50 up 14 min,  7 users,  load average: 0.02, 0.18, 0.17

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -21,10 +21,8 @@ int main() {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  fseek(fp, 0, SEEK_SET);
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("i=%d  n=%d\n",i,n);
     assert(n == i + 1);
   }
 
