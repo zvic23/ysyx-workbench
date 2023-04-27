3c8952b47f5250fee39a67f97c8c3bc707d31caa
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-27 10:30:20 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:30:20 up 18 min,  8 users,  load average: 0.37, 0.23, 0.19

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,7 +89,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 		if(len+position[fd] > f_size) len_fix = f_size - position[fd];
 		else len_fix = len;
 		ramdisk_read(buf, f_offset+position[fd], len_fix);
-		printf("fd: %d  position: %d  offset:%d   len: %d  len_fix: %d\n",fd,position[fd],f_offset,len,len_fix);
+		printf("read   fd: %d  position: %d  offset:%d   len: %d  len_fix: %d\n",fd,position[fd],f_offset,len,len_fix);
 		position[fd] += len_fix;
 		return len_fix;
 	}
@@ -105,6 +105,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
 		size_t len_fix = 0;
 		if(len+position[fd] > f_size) len_fix = f_size - position[fd];
 		ramdisk_write(buf, f_offset+position[fd], len_fix);
+		printf("write  fd: %d  position: %d  offset:%d   len: %d  len_fix: %d\n",fd,position[fd],f_offset,len,len_fix);
 		position[fd] += len_fix;
 		return len_fix;
 	}
