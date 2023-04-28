4bd2333f57e519726936ff8346ef3852f86cbf9e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-28 12:01:42 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:01:42 up  2:16, 13 users,  load average: 0.05, 0.11, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -101,7 +101,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   	          fs_lseek(fd, p_offset, SEEK_SET);
   	          fs_read(fd, addr, p_filesz);
 		  //memcpy(addr, &ramdisk_start+p_offset+400143, p_filesz);
-		  memset(addr+p_filesz, 0, p_memsz-p_filesz);
+		  memset((void*)(p_vaddr+p_filesz), 0, p_memsz-p_filesz);
 		  printf("mem : %p\n",addr+p_filesz+1);
 		  printf("mem : %lx\n",*(uint64_t*)(addr+p_filesz+1));
 		  printf("%d load over\n",i);
