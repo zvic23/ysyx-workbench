2f039eab5f5afc004e03e116d9e5cf7d87cef272
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-29 21:07:57 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:57 up  2:20, 17 users,  load average: 0.72, 0.72, 0.52

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -129,7 +129,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 	  uint32_t *palette =(uint32_t*)(s->format->palette->colors);
 	printf("rect in \n");
 	  for(int i=0;i<s->w*s->h;i++){
-		  //pixels_fix[i] = palette[((uint8_t*)s->pixels)[i]];
+		  pixels_fix[i] = palette[((uint8_t*)s->pixels)[i]];
 	  }
 	printf("rect in \n");
 	  pixels = pixels_fix;
