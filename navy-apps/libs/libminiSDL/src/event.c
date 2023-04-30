1e86e75e3cf007079e44f311df494f171cf829ab
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 15:25:16 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:16 up 22 min,  6 users,  load average: 0.41, 0.22, 0.22

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -10,6 +10,20 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static uint64_t get_keyvalue(char *keychar){
+  if     (strcmp(keychar, "J")==0)       return  SDLK_J;
+  else if(strcmp(keychar, "K")==0)       return  SDLK_K;
+  else if(strcmp(keychar, "G")==0)       return  SDLK_G;
+  else if(strcmp(keychar, "0")==0)       return  SDLK_0;
+  else if(strcmp(keychar, "1")==0)       return  SDLK_1;
+  else if(strcmp(keychar, "2")==0)       return  SDLK_2;
+  else if(strcmp(keychar, "RIGHT")==0)   return  SDLK_RIGHT;
+  else if(strcmp(keychar, "LEFT")==0)    return  SDLK_LEFT;
+  else if(strcmp(keychar, "SPACE")==0)   return  SDLK_SPACE;
+  else if(strcmp(keychar, "RETURN")==0)  return  SDLK_RETURN;
+  else return -1;
+}
+
 int SDL_PushEvent(SDL_Event *ev) {
 	printf("sdl not implement!\n");
 	assert(0);
@@ -38,7 +52,8 @@ int SDL_PollEvent(SDL_Event *ev) {
   else if(strcmp(event1, "LEFT")==0)  ev->key.keysym.sym = SDLK_LEFT;
   else if(strcmp(event1, "SPACE")==0)  ev->key.keysym.sym = SDLK_SPACE;
   else if(strcmp(event1, "RETURN")==0)  ev->key.keysym.sym = SDLK_RETURN;
-  //else e.type = SDL_KEYUP;
+
+
   if(ev->type == SDL_KEYDOWN || ev->type == SDL_KEYUP) return 1;
   else return 0;
 	//printf("sdl not implement!\n");
@@ -60,18 +75,19 @@ int SDL_WaitEvent(SDL_Event *event) {
   if(strcmp(event0, "kd")==0) event->type = SDL_KEYDOWN;
   else if(strcmp(event0, "ku")==0) event->type = SDL_KEYUP;
   else event->type = SDL_KEYUP;
-  
-  if(strcmp(event1, "J")==0) event->key.keysym.sym = SDLK_J;
-  else if(strcmp(event1, "K")==0) event->key.keysym.sym = SDLK_K;
-  else if(strcmp(event1, "G")==0) event->key.keysym.sym = SDLK_G;
-  else if(strcmp(event1, "0")==0) event->key.keysym.sym = SDLK_0;
-  else if(strcmp(event1, "1")==0) event->key.keysym.sym = SDLK_1;
-  else if(strcmp(event1, "2")==0) event->key.keysym.sym = SDLK_2;
-  else if(strcmp(event1, "RIGHT")==0) event->key.keysym.sym = SDLK_RIGHT;
-  else if(strcmp(event1, "LEFT")==0)  event->key.keysym.sym = SDLK_LEFT;
-  else if(strcmp(event1, "SPACE")==0)  event->key.keysym.sym = SDLK_SPACE;
-  else if(strcmp(event1, "RETURN")==0)  event->key.keysym.sym = SDLK_RETURN;
-  //else e.type = SDL_KEYUP;
+ 
+  event->key.keysym.sym = get_keyvalue(event1);;
+
+//  if(strcmp(event1, "J")==0) event->key.keysym.sym = SDLK_J;
+//  else if(strcmp(event1, "K")==0) event->key.keysym.sym = SDLK_K;
+//  else if(strcmp(event1, "G")==0) event->key.keysym.sym = SDLK_G;
+//  else if(strcmp(event1, "0")==0) event->key.keysym.sym = SDLK_0;
+//  else if(strcmp(event1, "1")==0) event->key.keysym.sym = SDLK_1;
+//  else if(strcmp(event1, "2")==0) event->key.keysym.sym = SDLK_2;
+//  else if(strcmp(event1, "RIGHT")==0) event->key.keysym.sym = SDLK_RIGHT;
+//  else if(strcmp(event1, "LEFT")==0)  event->key.keysym.sym = SDLK_LEFT;
+//  else if(strcmp(event1, "SPACE")==0)  event->key.keysym.sym = SDLK_SPACE;
+//  else if(strcmp(event1, "RETURN")==0)  event->key.keysym.sym = SDLK_RETURN;
 
 
 	//printf("sdl not implement!\n");
@@ -113,17 +129,4 @@ uint8_t* SDL_GetKeyState(int *numkeys) {
 	printf("getkeystate sdl not implement!\n");
 	assert(0);
   return NULL;
-}
-
-static uint64_t get_keyvalue(char *keychar){
-  if     (strcmp(keychar, "J")==0)       return  SDLK_J;
-  else if(strcmp(keychar, "K")==0)       return  SDLK_K;
-  else if(strcmp(keychar, "G")==0)       return  SDLK_G;
-  else if(strcmp(keychar, "0")==0)       return  SDLK_0;
-  else if(strcmp(keychar, "1")==0)       return  SDLK_1;
-  else if(strcmp(keychar, "2")==0)       return  SDLK_2;
-  else if(strcmp(keychar, "RIGHT")==0)   return  SDLK_RIGHT;
-  else if(strcmp(keychar, "LEFT")==0)    return  SDLK_LEFT;
-  else if(strcmp(keychar, "SPACE")==0)   return  SDLK_SPACE;
-  else if(strcmp(keychar, "RETURN")==0)  return  SDLK_RETURN;
 }
