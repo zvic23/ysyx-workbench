ebea2e7f197250fe5ec0ea5ebf2382fe39d2055c
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-03 21:21:02 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:02 up  2:50, 13 users,  load average: 0.10, 0.17, 0.17

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,8 +89,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 		if(len+position[fd] > f_size) len_fix = f_size - position[fd];
 		else len_fix = len;
 		ramdisk_read(buf, f_offset+position[fd], len_fix);
-		int a =1;
-		while(a--);
+		printf("");
 		//printf("read   fd: %d  position: %d  offset:%d   len: %d  len_fix: %d\n",fd,position[fd],f_offset,len,len_fix);
 		position[fd] += len_fix;
 		return len_fix;
