ffb525e59405393761e0f4b32e4076f4c4b3c294
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-03 10:33:59 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:59 up  1:30,  9 users,  load average: 0.49, 0.35, 0.21

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -123,6 +123,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   extern int detach_difftest;
   if(detach_difftest == 1) return;
+  printf("in\n");
 
   CPU_state ref_r;
 
