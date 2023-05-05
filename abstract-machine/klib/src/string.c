4ad29cc94592f61ffe4ffce4a9c5857f2540dc78
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 19:35:28 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:28 up  4:00,  9 users,  load average: 0.07, 0.17, 0.40

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -78,7 +78,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 void *memcpy(void *out, const void *in, size_t n) {
   uint8_t *dest = (uint8_t*)out;
   uint8_t *src  = (uint8_t*)in ;
-  size_t i=0;
+  int i=0;
   for(i=0; i<n ; i++){
 	  dest[i]=src[i];
   }
