fc0a7e21b9f80ba092296559f2bd4d4e31274b2a
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 18:53:20 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:53:20 up  3:18,  7 users,  load average: 1.11, 0.83, 0.43

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -177,7 +177,7 @@ static int decode_exec(Decode *s) {
 //	if(BITS(src1,31,31)!=BITS(src2,31,31))quotient=(quotient^0xffffffff)+1;//|0x80000000;
 //	R(dest) = SEXT(quotient,32));               //!!!!!have doubt and to be optimizied 
 						    
-  INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, sword_t src1_32s=BITS(src1,31,0);sword_t src2_32s=BITS(src2,31,0);sword_t remainder=src1_32s%src2_32s;R(dest) = SEXT(remainder,32)); 
+  INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, sword_t src1_32s=BITS(src1,31,0)<<32;sword_t src2_32s=BITS(src2,31,0)<<32;sword_t remainder=src1_32s%src2_32s;R(dest) = SEXT(remainder>>32,32)); 
 //  INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, word_t src1_32=BITS(src1,31,0);word_t src2_32=BITS(src2,31,0);word_t remainder=0;word_t src1_32_cpl=(src1_32^0xffffffff)+1; word_t src2_32_cpl=(src2_32^0xffffffff)+1; 
 //	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==0)remainder=src1_32%src2_32;
 //	if(BITS(src1,31,31)==BITS(src2,31,31)&&BITS(src1,31,31)==1)remainder=src1_32_cpl%src2_32_cpl;
