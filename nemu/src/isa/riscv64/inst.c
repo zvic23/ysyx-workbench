f5576388b11540714ce23cf60cf51c0b61cfd412
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 16:28:03 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:28:03 up 53 min,  7 users,  load average: 0.05, 0.15, 0.27

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -191,10 +191,11 @@ static int decode_exec(Decode *s) {
 //	if(BITS(src1,63,63)==BITS(src2,63,63))match=(src1<src2)?1:0;
 //	if(BITS(src1,63,63)!=BITS(src2,63,63))match=(BITS(src1,63,63)==1)?1:0;
 //	s->dnpc = (match)?(s->pc+imm):s->dnpc);     //!!!!!have doubt and to be optimized
-  INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, word_t match=0;
-	if(BITS(src1,63,63)==BITS(src2,63,63))match=(src1<src2)?1:0;
-	if(BITS(src1,63,63)!=BITS(src2,63,63))match=(BITS(src1,63,63)==1)?1:0;
-	R(dest) = (match)?1:0);     //!!!!!to be optimized
+  INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, sword_t src1_s=src1; sword_t src2_s=src2;R(dest) = (src1_s<src2_s)?1:0);
+//  INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, word_t match=0;
+//	if(BITS(src1,63,63)==BITS(src2,63,63))match=(src1<src2)?1:0;
+//	if(BITS(src1,63,63)!=BITS(src2,63,63))match=(BITS(src1,63,63)==1)?1:0;
+//	R(dest) = (match)?1:0);     //!!!!!to be optimized
 
   INSTPAT("??????? ????? ????? 001 ????? 00000 11", lh     , I, R(dest) = SEXT(Mr(src1 + imm, 2),16));
   INSTPAT("??????? ????? ????? 101 ????? 00000 11", lhu    , I, R(dest) = Mr(src1 + imm, 2));
