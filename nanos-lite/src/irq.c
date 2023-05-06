fc8f6584a126e57e24cc112935818fe76fe755db
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 15:36:57 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:57 up 36 min,  9 users,  load average: 0.16, 0.19, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,8 +5,8 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_SYSCALL: do_syscall(c); break;
-    //case EVENT_YIELD: printf("yield happened!\n"); break;
-    default: panic("Unhandled event ID = %d  %d", e.event, EVENT_SYSCALL);
+    case EVENT_YIELD: printf("yield happened!\n"); break;
+    default: panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
