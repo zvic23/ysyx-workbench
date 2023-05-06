63750d1c93eff7b124996ea4841f7a51434c5b04
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 19:15:43 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:15:43 up 44 min,  7 users,  load average: 0.21, 0.20, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -15,15 +15,10 @@
 #include "include/generated/autoconf.h"
 #include "include/common.h"
 
-
 #include "verilated_dpi.h"            //zsl:for printf the gpr
 
-const char *regs_name[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6","pc"
-};
+
+//*******************************************************************************
 
 //VerilatedContext* contextp = NULL;
 //VerilatedVcdC* tfp = NULL;
@@ -49,6 +44,8 @@ void sim_exit(){
   //step_and_dump_wave();
   //tfp->close();
 }
+
+//*******************************************************************************
 
 
 int skip_difftest=0;
@@ -143,7 +140,7 @@ void built_in_program(){
 }
 
 
-long img_size;
+uint64_t img_size;
 void load_img(){
   FILE *fp = fopen("./csrc/obj.bin", "rb");
 
