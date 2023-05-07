82af8f8faddf6bfa1e5258ca63375316e19b2101
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-07 09:41:03 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:41:03 up 41 min, 10 users,  load average: 0.05, 0.07, 0.08

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,11 @@
 #include <am.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint16_t keyvalue = inw(KBD_ADDR);
+  if( (keyvalue & KEYDOWN_MASK) == 0x8000) kbd->keydown = 1;
+  else kbd->keydown = 0;
+  kbd->keycode = keyvalue & ~KEYDOWN_MASK;
+
+//  kbd->keydown = 0;
+//  kbd->keycode = AM_KEY_NONE;
 }
