fdf089aa437b93c2acbeb68ab577d4ee043b0058
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-07 10:16:02 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:16:02 up  1:16, 13 users,  load average: 0.47, 0.38, 0.24

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -5,7 +5,7 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //uint16_t keyvalue = inw(KBD_ADDR);
-  uint16_t *KBD_ADDR = (uint16_t*)0xa0000060;
+  uint16_t *KBD_ADDR = (uint16_t*)0xa0000050;
   uint16_t keyvalue = KBD_ADDR[0];
 //printf("am key:%x\n",keyvalue);
   if( (keyvalue & KEYDOWN_MASK) == 0x8000) kbd->keydown = 1;
