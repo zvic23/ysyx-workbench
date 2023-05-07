3b95160d778416d95eae134211d64290eac57856
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-07 10:09:17 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:09:17 up  1:09, 13 users,  load average: 0.20, 0.19, 0.13

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -38,8 +38,8 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
 		return;
 	}
 	else if(raddr == 0xa0000060){                    //keyboard support
-		printf("in!!!!!!!!!!!!!!!!!!!\n");
 		uint64_t key = i8042_data_io_handler();
+		printf("key = %lx\n",key);
 		memcpy(rdata, &key, 8);
 	}
 
