af9f2c8b04b8938d2e149be0fd0d7ddbdeb76604
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-08 20:58:36 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:36 up  2:07, 25 users,  load average: 0.14, 0.13, 0.09

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -75,6 +75,7 @@ void difftest_raise_intr(word_t NO) {
 }
 
 void difftest_init(int port) {
+	init_mem();   //zsl: at <memory/paddr.h>, I add the function's declare.
 
 
   /* Perform ISA dependent initialization. */
