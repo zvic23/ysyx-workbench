f76925a33b75dd488a273e447f3eb6b0f4061c41
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-09 10:43:27 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:27 up  1:14, 11 users,  load average: 0.81, 0.99, 0.98

--- npc/csrc/infrastructure/dut.c
+++ npc/csrc/infrastructure/dut.c
@@ -64,9 +64,40 @@ free(resetmem);
 
 }
 
+
+void syn_state_to_ref(){    //zsl:i add this function
+//  CPU_state csr_buf;
+  uint64_t csr_buf[33];
+//extern uint64_t mepc,mcause,mstatus;
+//extern uint64_t mtvec;
+  csr_buf[10] = mtvec;    csr_buf[11] = mcause;  
+  csr_buf[12] = mstatus;  csr_buf[13] = mepc;
+  csr_buf[32] = RESET_VECTOR;
+  //printf("csr: %lx  %lx  %lx  %lx\n",mtvec,mcause,mstatus,mepc);
+  uint32_t buf[4];
+  buf[0]=0x30551073;     //  0x305 01010 001 00000 1110011    0x30551073
+  buf[1]=0x34259073;     //  0x342 01011 001 00000 1110011    0x34259073
+  buf[2]=0x30061073;     //  0x300 01100 001 00000 1110011    0x30061073
+  buf[3]=0x34169073;     //  0x341 01101 001 00000 1110011    0x34169073
+  ref_difftest_memcpy(0x80000000, buf, 16, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&csr_buf, DIFFTEST_TO_REF);
+  ref_difftest_exec(4);
+
+  ref_difftest_memcpy(0x80000000, pmem, 0x7ffffff, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&cpu_gpr_set, DIFFTEST_TO_REF);
+}
+
+
+
+
+
+
 extern const char *regs[];
 extern int npc_state;
 void difftest_step() {
+  extern int detach_difftest;
+  if(detach_difftest == 1) return;
+
   uint64_t ref_r[33];
 
   ref_difftest_exec(1);
