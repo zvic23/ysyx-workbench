ff8d4dce0aedb73d49702b63eac734d93b33bd28
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-05 15:33:21 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:33:21 up 51 min,  8 users,  load average: 0.32, 0.23, 0.23

--- npc/obj_dir/Vysyx_22050612_npc___024root__Slow.cpp
+++ npc/obj_dir/Vysyx_22050612_npc___024root__Slow.cpp
@@ -1910,7 +1910,7 @@ void Vysyx_22050612_npc___024root___settle__TOP__3(Vysyx_22050612_npc___024root*
         } else if (vlSelf->ysyx_22050612_npc__DOT__rvalid) {
             vlSelf->ysyx_22050612_npc__DOT__sram__DOT__read_next_state = 0U;
         }
-    } else if (VL_UNLIKELY((1U == (IData)(vlSelf->ysyx_22050612_npc__DOT__sram__DOT__read_current_state)))) {
+    } else if ((1U == (IData)(vlSelf->ysyx_22050612_npc__DOT__sram__DOT__read_current_state))) {
         Vysyx_22050612_npc___024unit____Vdpiimwrap_pmem_read_TOP____024unit((QData)((IData)(ysyx_22050612_npc__DOT__araddr)), vlSelf->__Vtask_pmem_read__20__rdata);
         vlSelf->ysyx_22050612_npc__DOT__rvalid = 1U;
         vlSelf->ysyx_22050612_npc__DOT__rdata = vlSelf->__Vtask_pmem_read__20__rdata;
@@ -1920,7 +1920,9 @@ void Vysyx_22050612_npc___024root___settle__TOP__3(Vysyx_22050612_npc___024root*
         } else if (vlSelf->ysyx_22050612_npc__DOT__rvalid) {
             vlSelf->ysyx_22050612_npc__DOT__sram__DOT__read_next_state = 0U;
         }
-        VL_WRITEF("data:%x\n",64,vlSelf->ysyx_22050612_npc__DOT__rdata);
+        if (VL_UNLIKELY((0xa0000060U == ysyx_22050612_npc__DOT__araddr))) {
+            VL_WRITEF("data:%x\n",64,vlSelf->ysyx_22050612_npc__DOT__rdata);
+        }
     } else if ((3U == (IData)(vlSelf->ysyx_22050612_npc__DOT__sram__DOT__read_current_state))) {
         vlSelf->ysyx_22050612_npc__DOT__rvalid = 0U;
         vlSelf->ysyx_22050612_npc__DOT__rresp = 0U;
