87dd41e6e2d8aae18ed87090c92064aaa2fee8d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-20 17:08:38 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:08:38 up  2:15, 10 users,  load average: 0.08, 0.02, 0.03

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -134,10 +134,10 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
 
 
 //void built_in_program(){
-//  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000004]=0x00208113; //gpr[2](sp)+2
-//  *(uint32_t*)&pmem[0x00000008]=0x00310193; //gpr[3](gp)+3
-//  *(uint32_t*)&pmem[0x0000000c]=0x00418213; //gpr[4](tp)+4
+//  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+gpr[0]1
+//  *(uint32_t*)&pmem[0x00000004]=0x00208113; //gpr[2](sp)+gpr[1]2
+//  *(uint32_t*)&pmem[0x00000008]=0x00310193; //gpr[3](gp)+gpr[2]3
+//  *(uint32_t*)&pmem[0x0000000c]=0x00418213; //gpr[4](tp)+gpr[3]4
 //  *(uint32_t*)&pmem[0x00000010]=0x00100073; //ebreak
 //}
 
