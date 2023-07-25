a052ab81c8421ec06fd59166beb573b0300b4b46
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-25 09:40:52 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:40:52 up 44 min,  7 users,  load average: 0.10, 0.05, 0.01

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -147,10 +147,10 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
 //  *(uint32_t*)&pmem[0x00000008]=0x00500193; //gpr[1](ra)+1
 //  *(uint32_t*)&pmem[0x0000000c]=0x00700213; //gpr[1](ra)+1
 //					    //
-//  *(uint32_t*)&pmem[0x00000010]=0x00100093; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000014]=0x00100093; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000018]=0x00100093; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x0000001c]=0x00100093; //gpr[1](ra)+1
+//  *(uint32_t*)&pmem[0x00000010]=0x00100013; //gpr[1](ra)+1
+//  *(uint32_t*)&pmem[0x00000014]=0x00100013; //gpr[1](ra)+1
+//  *(uint32_t*)&pmem[0x00000018]=0x00100013; //gpr[1](ra)+1
+//  *(uint32_t*)&pmem[0x0000001c]=0x00100013; //gpr[1](ra)+1
 //					    //
 //  *(uint32_t*)&pmem[0x00000020]=0x021102b3; //gpr[1](ra)+1
 //  *(uint32_t*)&pmem[0x00000024]=0x02218333; //gpr[2](sp)+2
@@ -174,7 +174,10 @@ void built_in_program(){
   *(uint32_t*)&pmem[0x00000024]=0x02218333; //gpr[2](sp)+2
   *(uint32_t*)&pmem[0x00000028]=0x023203b3; //gpr[3](gp)+3
   *(uint32_t*)&pmem[0x0000002c]=0x02408433; //gpr[4](tp)+4
-  *(uint32_t*)&pmem[0x00000030]=0x00100073; //ebreak
+					    //
+  *(uint32_t*)&pmem[0x00000030]=0x0020056f; //jal
+
+  *(uint32_t*)&pmem[0x00000034]=0x00100073; //ebreak
 }
 
 
