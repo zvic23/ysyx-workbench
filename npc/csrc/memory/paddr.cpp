5c0aecf9ce69b20b85ab9e601608e6412d627480
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-25 17:07:39 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:39 up  2:14, 12 users,  load average: 0.12, 0.06, 0.09

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -52,11 +52,12 @@ extern "C" void pmem_read_pc(long long raddr, long long *rdata) {
 	uint64_t* inst_64;
 	memcpy(inst_64, &pmem[raddr_set-0x80000000], 8);
 	uint32_t inst = 0;
+	/*
 	if((raddr_set>>2)&1 == 1){
 		inst = (*inst_64)>>32;
 	}else{
 		inst= (uint32_t)*inst_64;
-	}
+	}*/
         itrace(top->pc, inst);
   }
 }
