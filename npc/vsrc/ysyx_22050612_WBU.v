e815898f57ab25bc6b6ad4012051f834224120cb
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-26 16:12:58 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:12:58 up  1:15, 14 users,  load average: 0.12, 0.04, 0.05

--- npc/vsrc/ysyx_22050612_WBU.v
+++ npc/vsrc/ysyx_22050612_WBU.v
@@ -48,12 +48,13 @@ end
 //assign opcode = WB_reg_valid ? WB_reg_opcode : 24'b0;
 
 
-
+reg [63:0]pc_last;
 
 always @(negedge clk) begin
 	//$display("WB   pc:%x   inst:%x",WB_reg_pc,WB_reg_inst);
-	if(WB_reg_valid) begin 
+	if(pc_last != WB_reg_pc) begin 
 		npc_complete_one_inst();
+		pc_last <= WB_reg_pc;
 	end
 end
 //********************************************************************
