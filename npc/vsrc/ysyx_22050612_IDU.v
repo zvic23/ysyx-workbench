84cfe13548821085725d3c85477f3945357653ce
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-26 16:39:38 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:39:38 up  1:42, 14 users,  load average: 0.04, 0.06, 0.06

--- npc/vsrc/ysyx_22050612_IDU.v
+++ npc/vsrc/ysyx_22050612_IDU.v
@@ -266,6 +266,14 @@ always @(*) begin
     24'd8    : src2_conflict = gpr_busy[rs2] == 1'b1;  
     24'd9    : src2_conflict = gpr_busy[rs2] == 1'b1;  
     24'd10   : src2_conflict = gpr_busy[rs2] == 1'b1;  
+
+
+//sd  sb  sh  sw have imm and two src, and the same time alu just use src1 and
+//imm, so it need to add src2 to check conflict
+    24'd43   : src2_conflict = gpr_busy[rs2] == 1'b1; 
+
+
+
 //    24'd50   : ALU_operator_b=src_csr;
 
 //mul / div
