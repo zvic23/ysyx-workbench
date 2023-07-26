1f5b5155529a978cb43fa376285a93c9dedc18a9
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-26 15:17:34 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:17:34 up 20 min,  8 users,  load average: 0.11, 0.12, 0.11

--- npc/vsrc/ysyx_22050612_npc.v
+++ npc/vsrc/ysyx_22050612_npc.v
@@ -56,7 +56,7 @@ always@(posedge clk) begin
 	if(gpr_rd != 5'b0 && gpr_wen == 1'b1 ) begin
 		gpr_busy[gpr_rd] <= 1'b0;
 	end
-	if(rd != 5'b0 && wen == 1'b1 && ready_IF_ID == 1'b1 ) begin
+	else if(rd != 5'b0 && wen == 1'b1 && ready_IF_ID == 1'b1 ) begin
 		gpr_busy[rd]     <= 1'b1;
 	end
 end
