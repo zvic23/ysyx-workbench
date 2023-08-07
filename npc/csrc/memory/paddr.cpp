f07d9e927ee0c9f0d9887b501e3d55509b4bc8c6
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-07 15:46:35 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:35 up  1:45, 10 users,  load average: 0.28, 0.20, 0.15

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -152,31 +152,31 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
 
 
 
-void built_in_program(){
-  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+gpr[0]1
-  *(uint32_t*)&pmem[0x00000004]=0x00208113; //gpr[2](sp)+gpr[1]2
-  *(uint32_t*)&pmem[0x00000008]=0x00310193; //gpr[3](gp)+gpr[2]3
-  *(uint32_t*)&pmem[0x0000000c]=0x00418213; //gpr[4](tp)+gpr[3]4
-  *(uint32_t*)&pmem[0x00000010]=0x00100073; //ebreak
-}
-
 //void built_in_program(){
-//  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000004]=0x00300113; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000008]=0x00500193; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x0000000c]=0x00700213; //gpr[1](ra)+1
-//					    //
-//  *(uint32_t*)&pmem[0x00000010]=0x00100013; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000014]=0x00100013; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000018]=0x00100013; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x0000001c]=0x00100013; //gpr[1](ra)+1
-//					    //
-//  *(uint32_t*)&pmem[0x00000020]=0x021102b3; //gpr[1](ra)+1
-//  *(uint32_t*)&pmem[0x00000024]=0x02218333; //gpr[2](sp)+2
-//  *(uint32_t*)&pmem[0x00000028]=0x023203b3; //gpr[3](gp)+3
-//  *(uint32_t*)&pmem[0x0000002c]=0x02408433; //gpr[4](tp)+4
-//  *(uint32_t*)&pmem[0x00000030]=0x00100073; //ebreak
+//  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+gpr[0]1
+//  *(uint32_t*)&pmem[0x00000004]=0x00208113; //gpr[2](sp)+gpr[1]2
+//  *(uint32_t*)&pmem[0x00000008]=0x00310193; //gpr[3](gp)+gpr[2]3
+//  *(uint32_t*)&pmem[0x0000000c]=0x00418213; //gpr[4](tp)+gpr[3]4
+//  *(uint32_t*)&pmem[0x00000010]=0x00100073; //ebreak
 //}
+
+void built_in_program(){
+  *(uint32_t*)&pmem[0x00000000]=0x00100093; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x00000004]=0x00300113; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x00000008]=0x00500193; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x0000000c]=0x00700213; //gpr[1](ra)+1
+					    //
+  *(uint32_t*)&pmem[0x00000010]=0x00100013; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x00000014]=0x00100013; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x00000018]=0x00100013; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x0000001c]=0x00100013; //gpr[1](ra)+1
+					    //
+  *(uint32_t*)&pmem[0x00000020]=0x021102b3; //gpr[1](ra)+1
+  *(uint32_t*)&pmem[0x00000024]=0x02218333; //gpr[2](sp)+2
+  *(uint32_t*)&pmem[0x00000028]=0x023203b3; //gpr[3](gp)+3
+  *(uint32_t*)&pmem[0x0000002c]=0x02408433; //gpr[4](tp)+4
+  *(uint32_t*)&pmem[0x00000030]=0x00100073; //ebreak
+}
 
 //void built_in_program(){
 //  *(uint32_t*)&pmem[0x00000000]=0x00108093; //ra = ra + 1
