1f360616253a56f75860e6307d74eb5e108aed7a
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-09 14:52:53 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:53 up  1:16, 10 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -204,7 +204,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
 
 void built_in_program(){        //check load interlock
   *(uint32_t*)&pmem[0x00000000]=0x00083083; //ra = ra + 1
-  *(uint32_t*)&pmem[0x00000004]=0x021102b3; //sp = sp + 3
+  *(uint32_t*)&pmem[0x00000004]=0x00108133; //sp = sp + 3
   *(uint32_t*)&pmem[0x00000008]=0x00518193; //gp = gp + 5
   *(uint32_t*)&pmem[0x0000000c]=0x00720213; //tp = tp + 7
 					    //
