a4e709c5413d137df6189b5a05f82709e7b58a35
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-10 11:22:13 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:22:13 up  2:25, 13 users,  load average: 0.06, 0.06, 0.06

--- npc/csrc/include/generated/autoconf.h
+++ npc/csrc/include/generated/autoconf.h
@@ -4,11 +4,4 @@
  * NPC Configuration Menu
  *
  */
-#define CONFIG_ITRACE 1
 #define CONFIG_DIFFTEST 1
-#define CONFIG_TRACE_END 10000
-#define CONFIG_MTRACE_START 0x80008f20
-#define CONFIG_MTRACE 1
-#define CONFIG_MTRACE_END 0x80008f24
-#define CONFIG_TRACE_START 0
-#define CONFIG_TRACE 1
