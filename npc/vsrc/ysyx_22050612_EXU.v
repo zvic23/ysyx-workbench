74c905b61372defb8351b08395687167580e7863
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 10:55:17 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:55:17 up  1:17, 11 users,  load average: 0.22, 0.28, 0.15

--- npc/vsrc/ysyx_22050612_EXU.v
+++ npc/vsrc/ysyx_22050612_EXU.v
@@ -869,70 +869,70 @@ always @(*) begin
 //    default: dnpc=snpc;
 //    endcase
 
-//    case (opcode)            // this edition is for always predict not jump
-//    24'h300 : dnpc=result_alu0                         ;
-//    24'd4   : dnpc={result_alu0[63:1],1'b0}            ;
-////    24'd5   : dnpc=(result_alu0==64'b0)?(imm_B+EX_reg_pc):snpc;
-////    24'd6   : dnpc=(result_alu0!=64'b0)?(imm_B+EX_reg_pc):snpc;
-////    24'd7   : dnpc=(result_alu0[63]==1)?(imm_B+EX_reg_pc):snpc;
-////    24'd8   : dnpc=(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc;
-//    24'd5   : dnpc=(src1==src2)?result_alu0:snpc;
-//    24'd6   : dnpc=(src1!=src2)?result_alu0:snpc;
-//    24'd7   : dnpc=($signed(src1) <$signed(src2))?result_alu0:snpc;
-//    24'd8   : dnpc=($signed(src1)>=$signed(src2))?result_alu0:snpc;
-//    24'd9   : dnpc=(src1 <src2)?result_alu0:snpc         ;
-//    24'd10  : dnpc=(src1>=src2)?result_alu0:snpc        ;        //(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc
-//    24'h200000: dnpc=EX_reg_src_b                             ;        
-//    24'h500000: dnpc=EX_reg_src_b                             ;        
-//    default: dnpc=snpc;
-//    endcase
-
-    case (opcode)
+    case (opcode)            // this edition is for always predict not jump
     24'h300 : dnpc=result_alu0                         ;
     24'd4   : dnpc={result_alu0[63:1],1'b0}            ;
 //    24'd5   : dnpc=(result_alu0==64'b0)?(imm_B+EX_reg_pc):snpc;
 //    24'd6   : dnpc=(result_alu0!=64'b0)?(imm_B+EX_reg_pc):snpc;
 //    24'd7   : dnpc=(result_alu0[63]==1)?(imm_B+EX_reg_pc):snpc;
 //    24'd8   : dnpc=(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc;
-    24'd5   : dnpc=(src1==src2&&EX_reg_inst[31]==0)?result_alu0:snpc;
-    24'd6   : dnpc=(src1!=src2&&EX_reg_inst[31]==0)?result_alu0:snpc;
-    24'd7   : dnpc=($signed(src1) <$signed(src2)&&EX_reg_inst[31]==0)?result_alu0:snpc;
-    24'd8   : dnpc=($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==0)?result_alu0:snpc;
-    24'd9   : dnpc=(src1 <src2&&EX_reg_inst[31]==0)?result_alu0:snpc         ;
-    24'd10  : dnpc=(src1>=src2&&EX_reg_inst[31]==0)?result_alu0:snpc        ;        //(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc
+    24'd5   : dnpc=(src1==src2)?result_alu0:snpc;
+    24'd6   : dnpc=(src1!=src2)?result_alu0:snpc;
+    24'd7   : dnpc=($signed(src1) <$signed(src2))?result_alu0:snpc;
+    24'd8   : dnpc=($signed(src1)>=$signed(src2))?result_alu0:snpc;
+    24'd9   : dnpc=(src1 <src2)?result_alu0:snpc         ;
+    24'd10  : dnpc=(src1>=src2)?result_alu0:snpc        ;        //(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc
     24'h200000: dnpc=EX_reg_src_b                             ;        
     24'h500000: dnpc=EX_reg_src_b                             ;        
     default: dnpc=snpc;
     endcase
 
-    case (opcode)
-    24'h300  : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
-    //24'h300  : pc_update= EX_reg_valid ? (EX_reg_inst[31]==1'b0 ? 1'b1 : 1'b0) : 1'b0;
-    24'd4    : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
-    24'd5    : pc_update= EX_reg_valid ? ( ((src1==src2&&EX_reg_inst[31]==0)||(src1!=src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'd6    : pc_update= EX_reg_valid ? ( ((src1!=src2&&EX_reg_inst[31]==0)||(src1==src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'd7    : pc_update= EX_reg_valid ? ( (($signed(src1) <$signed(src2)&&EX_reg_inst[31]==0)||($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'd8    : pc_update= EX_reg_valid ? ( (($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==0)||($signed(src1) <$signed(src2)&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'd9    : pc_update= EX_reg_valid ? ( ((src1 <src2&&EX_reg_inst[31]==0)||(src1>=src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'd10   : pc_update= EX_reg_valid ? ( ((src1>=src2&&EX_reg_inst[31]==0)||(src1 <src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
-    24'h200000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;   
-    24'h500000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;             
-    default: pc_update=1'b0;
-    endcase
+//    case (opcode)
+//    24'h300 : dnpc=result_alu0                         ;
+//    24'd4   : dnpc={result_alu0[63:1],1'b0}            ;
+////    24'd5   : dnpc=(result_alu0==64'b0)?(imm_B+EX_reg_pc):snpc;
+////    24'd6   : dnpc=(result_alu0!=64'b0)?(imm_B+EX_reg_pc):snpc;
+////    24'd7   : dnpc=(result_alu0[63]==1)?(imm_B+EX_reg_pc):snpc;
+////    24'd8   : dnpc=(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc;
+//    24'd5   : dnpc=(src1==src2&&EX_reg_inst[31]==0)?result_alu0:snpc;
+//    24'd6   : dnpc=(src1!=src2&&EX_reg_inst[31]==0)?result_alu0:snpc;
+//    24'd7   : dnpc=($signed(src1) <$signed(src2)&&EX_reg_inst[31]==0)?result_alu0:snpc;
+//    24'd8   : dnpc=($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==0)?result_alu0:snpc;
+//    24'd9   : dnpc=(src1 <src2&&EX_reg_inst[31]==0)?result_alu0:snpc         ;
+//    24'd10  : dnpc=(src1>=src2&&EX_reg_inst[31]==0)?result_alu0:snpc        ;        //(result_alu0[63]==0)?(imm_B+EX_reg_pc):snpc
+//    24'h200000: dnpc=EX_reg_src_b                             ;        
+//    24'h500000: dnpc=EX_reg_src_b                             ;        
+//    default: dnpc=snpc;
+//    endcase
 
-//    case (opcode)    // this edition is for always predict not jump
+//    case (opcode)
 //    24'h300  : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
+//    //24'h300  : pc_update= EX_reg_valid ? (EX_reg_inst[31]==1'b0 ? 1'b1 : 1'b0) : 1'b0;
 //    24'd4    : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
-//    24'd5    : pc_update= EX_reg_valid ? ((src1==src2)? 1'b1:1'b0) : 1'b0;
-//    24'd6    : pc_update= EX_reg_valid ? ((src1!=src2)? 1'b1:1'b0) : 1'b0;
-//    24'd7    : pc_update= EX_reg_valid ? (($signed(src1) <$signed(src2))? 1'b1:1'b0) : 1'b0;
-//    24'd8    : pc_update= EX_reg_valid ? (($signed(src1)>=$signed(src2))? 1'b1:1'b0) : 1'b0;
-//    24'd9    : pc_update= EX_reg_valid ? ((src1 <src2)? 1'b1:1'b0) : 1'b0;
-//    24'd10   : pc_update= EX_reg_valid ? ((src1>=src2)? 1'b1:1'b0) : 1'b0;
+//    24'd5    : pc_update= EX_reg_valid ? ( ((src1==src2&&EX_reg_inst[31]==0)||(src1!=src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
+//    24'd6    : pc_update= EX_reg_valid ? ( ((src1!=src2&&EX_reg_inst[31]==0)||(src1==src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
+//    24'd7    : pc_update= EX_reg_valid ? ( (($signed(src1) <$signed(src2)&&EX_reg_inst[31]==0)||($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
+//    24'd8    : pc_update= EX_reg_valid ? ( (($signed(src1)>=$signed(src2)&&EX_reg_inst[31]==0)||($signed(src1) <$signed(src2)&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
+//    24'd9    : pc_update= EX_reg_valid ? ( ((src1 <src2&&EX_reg_inst[31]==0)||(src1>=src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
+//    24'd10   : pc_update= EX_reg_valid ? ( ((src1>=src2&&EX_reg_inst[31]==0)||(src1 <src2&&EX_reg_inst[31]==1))? 1'b1:1'b0 ) : 1'b0;
 //    24'h200000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;   
 //    24'h500000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;             
 //    default: pc_update=1'b0;
 //    endcase
+
+    case (opcode)    // this edition is for always predict not jump
+    24'h300  : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
+    24'd4    : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
+    24'd5    : pc_update= EX_reg_valid ? ((src1==src2)? 1'b1:1'b0) : 1'b0;
+    24'd6    : pc_update= EX_reg_valid ? ((src1!=src2)? 1'b1:1'b0) : 1'b0;
+    24'd7    : pc_update= EX_reg_valid ? (($signed(src1) <$signed(src2))? 1'b1:1'b0) : 1'b0;
+    24'd8    : pc_update= EX_reg_valid ? (($signed(src1)>=$signed(src2))? 1'b1:1'b0) : 1'b0;
+    24'd9    : pc_update= EX_reg_valid ? ((src1 <src2)? 1'b1:1'b0) : 1'b0;
+    24'd10   : pc_update= EX_reg_valid ? ((src1>=src2)? 1'b1:1'b0) : 1'b0;
+    24'h200000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;   
+    24'h500000: pc_update=EX_reg_valid ? 1'b1 : 1'b0;             
+    default: pc_update=1'b0;
+    endcase
 
 //    case (opcode)
 //    24'h300  : pc_update= EX_reg_valid ? 1'b1 : 1'b0;
