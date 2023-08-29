0afe783d62f43c350d829f64c8d3ba8989a8abf8
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 11:42:30 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:30 up  2:04, 14 users,  load average: 0.08, 0.05, 0.06

--- npc/vsrc/ysyx_22050612_IFU.v
+++ npc/vsrc/ysyx_22050612_IFU.v
@@ -117,7 +117,7 @@ always @(*) begin
 		pc_next = pc+imm_B;
 		pc_en   = 1'b1;
 	end
-	else if(inst_is_branch==4'd2)begin
+	else if(inst_is_branch==4'd2 && minus_target_addr)begin
 		pc_next = pc+imm_J;
 		pc_en   = 1'b1;
 	end
@@ -129,16 +129,16 @@ end
 
 reg [3:0]inst_is_branch;
 always @(*) begin
-	if(inst[6:0] == 7'b1101111)begin
-		inst_is_branch = 4'd2;                                 //jal
-	end
+//	if(inst[6:0] == 7'b1101111)begin
+//		inst_is_branch = 4'd2;                                 //jal
+//	end
 //	else if(inst == 32'b1110011)begin
 //		//inst_is_branch = 4'd1;                                 //ecall
 //	end
 //	else if(inst == 32'b00110000001000000000000001110011)begin
 //		//inst_is_branch = 4'd1;                                 //mret
 //	end
-	else begin
+//	else begin
         	case ({inst[14:12],inst[6:0]})
         //	      10'b000_1100111: inst_is_branch = 4'd2;          //jalr
         	      10'b000_1100011: inst_is_branch = 4'd1;          //beq
@@ -149,7 +149,7 @@ always @(*) begin
         	      10'b111_1100011: inst_is_branch = 4'd1;          //bgeu
         	      default:         inst_is_branch = 4'd0; 
         	endcase
-	end
+//	end
 end
 
 wire minus_target_addr;
@@ -282,7 +282,7 @@ ysyx_22050612_Reg #(64,64'h80000000) pc_rg (clk, rst, pc_next, pc, pc_en);
 
 
 always @(negedge clk) begin
-	$display("IF   pc:%x   inst:%x   valid:%d",pc,inst,valid_IF_ID);
+	//$display("IF   pc:%x   inst:%x   valid:%d",pc,inst,valid_IF_ID);
 end
 
 
