3584c0bf7280254ab80d1729ef82c4236511c8b0
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-29 11:38:31 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:38:31 up  2:00, 14 users,  load average: 0.11, 0.06, 0.07

--- npc/vsrc/ysyx_22050612_IFU.v
+++ npc/vsrc/ysyx_22050612_IFU.v
@@ -117,10 +117,10 @@ always @(*) begin
 		pc_next = pc+imm_B;
 		pc_en   = 1'b1;
 	end
-	else if(inst_is_branch==4'd2)begin
-		pc_next = pc+imm_J;
-		pc_en   = 1'b1;
-	end
+//	else if(inst_is_branch==4'd2)begin
+//		pc_next = pc+imm_J;
+//		pc_en   = 1'b1;
+//	end
 	else begin
 		pc_next = pc + 64'd4;
 		pc_en   = 1'b1;
