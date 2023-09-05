d9c6838ae5c5cec2aacb9777c781299bdab3f53d
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-05 16:21:10 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:10 up  5:29, 15 users,  load average: 0.08, 0.06, 0.01

--- npc/vsrc/ysyx_22050612_EXU.v
+++ npc/vsrc/ysyx_22050612_EXU.v
@@ -226,10 +226,10 @@ wire rs1_EX_MEM_match;
 wire rs2_EX_MEM_match;
 wire rs1_EX_WB_match;
 wire rs2_EX_WB_match;
-assign rs1_EX_MEM_match = MEM_reg_inst[11:7] == EX_reg_inst[19:15];
-assign rs2_EX_MEM_match = MEM_reg_inst[11:7] == EX_reg_inst[24:20];
-assign rs1_EX_WB_match  =  WB_reg_inst[11:7] == EX_reg_inst[19:15];
-assign rs2_EX_WB_match  =  WB_reg_inst[11:7] == EX_reg_inst[24:20];
+assign rs1_EX_MEM_match = (MEM_reg_inst[11:7] == EX_reg_inst[19:15])&&(EX_reg_inst[19:15]!=5'b0);
+assign rs2_EX_MEM_match = (MEM_reg_inst[11:7] == EX_reg_inst[24:20])&&(EX_reg_inst[24:20]!=5'b0);
+assign rs1_EX_WB_match  = ( WB_reg_inst[11:7] == EX_reg_inst[19:15])&&(EX_reg_inst[19:15]!=5'b0);
+assign rs2_EX_WB_match  = ( WB_reg_inst[11:7] == EX_reg_inst[24:20])&&(EX_reg_inst[24:20]!=5'b0);
 
 wire [3:0]MEM_inst_hit;
 wire [3:0]WB_inst_hit;
