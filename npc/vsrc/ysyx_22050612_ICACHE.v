318415addfbdd21dab7fbd7bfa4fe983d75d81c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-14 16:51:26 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:51:26 up  2:56, 16 users,  load average: 0.06, 0.05, 0.07

--- npc/vsrc/ysyx_22050612_ICACHE.v
+++ npc/vsrc/ysyx_22050612_ICACHE.v
@@ -31,7 +31,7 @@ reg [63:0]v3;
 //************************  pipeline  ******************************
 always @(negedge clk) begin
 	$display("icache   pc:%x   inst:%x   valid:%d   ready:%d   line:%x  index:%x  index_prev:%x  offset:%x  offset_prev:%x",addr_prev,inst,valid,ready,line_mem_prev,index,addr_prev[9:4],addr[3:0],addr_prev[3:0]);
-	$display("icache   %b   %b    %d  %d  %d  %d   dout:%x  dout0:%x",way_hit,way_hit_prev,cen0,cen1,cen2,cen3,dout,dout0);
+	$display("icache   %b   %b    %d  %d  %d  %d   dout:%x  dout0:%x   wen:%x",way_hit,way_hit_prev,cen0,cen1,cen2,cen3,dout,dout0,wen);
 end
 //*****************************************************************
 always @(posedge clk) begin
