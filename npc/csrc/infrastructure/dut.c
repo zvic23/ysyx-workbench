5e7d5ef176543e5fa6c177cb87197910ac289ec2
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-14 14:30:37 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:30:37 up 35 min,  8 users,  load average: 0.01, 0.02, 0.04

--- npc/csrc/infrastructure/dut.c
+++ npc/csrc/infrastructure/dut.c
@@ -98,12 +98,11 @@ extern int detach_difftest;
 void difftest_step() {
   if(detach_difftest == 1) return;
 
+  ref_difftest_exec(1);
   uint64_t ref_r_old[33];
   ref_difftest_regcpy(&ref_r_old, DIFFTEST_TO_DUT);
 
-	  printf("(pc)   npc.pc:%lx  nemu.pc:%lx\n",wb_pc,ref_r_old[32]);
   uint64_t ref_r[33];
-  ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
 
