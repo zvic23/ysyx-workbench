2d2b02c072deecdeba5f575dc09c3d8c4289789e
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-18 19:47:51 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:47:51 up  1:53, 15 users,  load average: 0.15, 0.04, 0.09

--- npc/csrc/cpu/npc-pipeline-trace.cpp
+++ npc/csrc/cpu/npc-pipeline-trace.cpp
@@ -117,13 +117,57 @@ void WBU_state_trace (long long a,long long b,long long c,long long d,long long 
 	F4[0] = f;
 }
 
+long long A5[20],B5[20],C5[20],D5[20],E5[20],F5[20],G5[20],H5[20],I5[20],J5[20],K5[20],L5[20],M5[20],N5[20],O5[20],P5[20];
+void ICACHE_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
+	long long z[19];
+	memcpy(z,A5,152);
+	memcpy(&A5[1],z,152);
+	A5[0] = a;
+	memcpy(z,B5,152);
+	memcpy(&B5[1],z,152);
+	B5[0] = b;
+	memcpy(z,C5,152);
+	memcpy(&C5[1],z,152);
+	C5[0] = c;
+	memcpy(z,D5,152);
+	memcpy(&D5[1],z,152);
+	D5[0] = d;
+	memcpy(z,E5,152);
+	memcpy(&E5[1],z,152);
+	E5[0] = e;
+	memcpy(z,F5,152);
+	memcpy(&F5[1],z,152);
+	F5[0] = f;
+	memcpy(z,G5,152);
+	memcpy(&G5[1],z,152);
+	G5[0] = g;
+	memcpy(z,H5,152);
+	memcpy(&H5[1],z,152);
+	H5[0] = h;
+	memcpy(z,I5,152);
+	memcpy(&I5[1],z,152);
+	I5[0] = i;
+	memcpy(z,J5,152);
+	memcpy(&J5[1],z,152);
+	J5[0] = j;
+	memcpy(z,K5,152);  memcpy(&K5[1],z,152);  K5[0] = k;
+	memcpy(z,L5,152);  memcpy(&L5[1],z,152);  K5[0] = l;
+	memcpy(z,M5,152);  memcpy(&M5[1],z,152);  K5[0] = m;
+	memcpy(z,N5,152);  memcpy(&N5[1],z,152);  K5[0] = n;
+	memcpy(z,O5,152);  memcpy(&O5[1],z,152);  K5[0] = o;
+	memcpy(z,P5,152);  memcpy(&P5[1],z,152);  K5[0] = p;
+	//memcpy(z,Q5,152);  memcpy(&Q5[1],z,152);  K5[0] = q;
+
+}
+
 void pipeline_state_printf(){
 	for(int i=19;i>=0;i--){
-		printf("IFU:   pc:%08llx  inst:%08llx  valid:%lld  ready:%lld\n",A[i],B[i],C[i],D[i]);
-		printf("IDU:   pc:%08llx  inst:%08llx  valid:%lld\n",A1[i],B1[i],C1[i]);
-		printf("EXU:   pc:%08llx  inst:%08llx  valid:%lld\n",A2[i],B2[i],C2[i]);
-		printf("MEM:   pc:%08llx  inst:%08llx  valid:%lld\n",A3[i],B3[i],C3[i]);
-		printf("WBU:   pc:%08llx  inst:%08llx  valid:%lld\n",A4[i],B4[i],C4[i]);
+		printf("IFU:   pc:%llx  inst:%08llx  valid:%lld  ready:%lld\n",A[i],B[i],C[i],D[i]);
+		printf("ICACHE:pc:%llx  inst:%08llx  valid:%lld  ready:%lld\n",A5[i],B5[i],C5[i],D5[i],E5[i],F5[i],G5[i],H5[i],I5[i],J5[i],K5[i],L5[i],M5[i],N5[i],O5[i],P5[i]);
+		printf("IDU:   pc:%llx  inst:%08llx  valid:%lld\n",A1[i],B1[i],C1[i]);
+		printf("EXU:   pc:%llx  inst:%08llx  valid:%lld\n",A2[i],B2[i],C2[i]);
+		printf("MEM:   pc:%llx  inst:%08llx  valid:%lld\n",A3[i],B3[i],C3[i]);
+		printf("WBU:   pc:%llx  inst:%08llx  valid:%lld\n",A4[i],B4[i],C4[i]);
 
 		printf("\n");
 	}
