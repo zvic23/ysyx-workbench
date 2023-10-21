1010b59ca12eee4201d7c5c9ea3cecad6517ad8b
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-21 16:37:21 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:37:21 up 49 min, 10 users,  load average: 0.15, 0.15, 0.28

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -68,14 +68,7 @@ extern "C" void pmem_read_dcache_low64(long long raddr, long long *rdata) {
 
   if(raddr>=0x80000000){
 	if(raddr == 0xa0000048){                         //rtc support
-		skip_difftest=1;
-		//printf("rtc     \n");
-		struct timeval time;
-		gettimeofday(&time,NULL);
-		uint64_t time_rtc = (time.tv_sec*1000000)+time.tv_usec - time_init;
-		//printf("time:%lx\n",time_rtc);
-		memcpy(rdata, &time_rtc, 8);
-		return;
+
 	}
 	else if(raddr == 0xa0000060){                    //keyboard support
 		skip_difftest=1;
@@ -96,7 +89,13 @@ extern "C" void pmem_read_dcache_high64(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0xfull`的8字节返回给`rdata`
   if(raddr>=0x80000000){
 	if(raddr == 0xa0000048){                         //rtc support
-
+		skip_difftest=1;
+		//printf("rtc     \n");
+		struct timeval time;
+		gettimeofday(&time,NULL);
+		uint64_t time_rtc = (time.tv_sec*1000000)+time.tv_usec - time_init;
+		//printf("time:%lx\n",time_rtc);
+		memcpy(rdata, &time_rtc, 8);
 		return;
 	}
 	else if(raddr == 0xa0000060){                    //keyboard support
