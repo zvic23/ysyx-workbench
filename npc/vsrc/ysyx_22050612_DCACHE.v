57a27e19efa7a51068a32d2ee5cf36d802adfbef
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-30 20:33:30 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:33:30 up  6:52, 12 users,  load average: 0.32, 0.19, 0.16

--- npc/vsrc/ysyx_22050612_DCACHE.v
+++ npc/vsrc/ysyx_22050612_DCACHE.v
@@ -291,10 +291,10 @@ wire [127:0]line_mem_wr;
 always @(negedge clk) begin
 		pmem_read_dcache_low64 (addr, line_mem[63:0]);
 		pmem_read_dcache_high64(addr, line_mem[127:64]);
-	//if(valid&&!ready)begin
+	if(valid&&!ready)begin
 		pmem_write_dcache_low64 (addr, wren, din, mask, line_mem_wr[63:0],line_mem_wr[127:64]);
 		//pmem_write_dcache_high64(addr, {7'b0,wren}, din, mask, line_mem_wr[127:64]);
-	//end
+	end
 end
 
 
