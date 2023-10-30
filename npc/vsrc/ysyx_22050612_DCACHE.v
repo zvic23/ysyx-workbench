9f86836e7a001cea7dfef3968543c38aa761c76d
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-30 17:43:42 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:42 up  6:30, 11 users,  load average: 0.08, 0.05, 0.35

--- npc/vsrc/ysyx_22050612_DCACHE.v
+++ npc/vsrc/ysyx_22050612_DCACHE.v
@@ -25,18 +25,25 @@ input [63:0]mask
 );
 
 
-reg [53:0]tag0[63:0];
-reg [53:0]tag1[63:0];
-reg [53:0]tag2[63:0];
-reg [53:0]tag3[63:0];
-reg [63:0]v0;
-reg [63:0]v1;
-reg [63:0]v2;
-reg [63:0]v3;
-
+//reg [53:0]tag0[63:0];
+//reg [53:0]tag1[63:0];
+//reg [53:0]tag2[63:0];
+//reg [53:0]tag3[63:0];
+//reg [63:0]v0;
+//reg [63:0]v1;
+//reg [63:0]v2;
+//reg [63:0]v3;
+reg [21:0]tag0[15:0];
+reg [21:0]tag1[15:0];
+reg [21:0]tag2[15:0];
+reg [21:0]tag3[15:0];
+reg [15:0]v0;
+reg [15:0]v1;
+reg [15:0]v2;
+reg [15:0]v3;
 //************************  pipeline  ******************************
 always @(negedge clk) begin
-	DCACHE_state_trace (addr, dout, {63'b0,valid}, {63'b0,ready}, line_mem_prev[127:64], line_mem_prev[63:0], {58'b0,index}, {58'b0,addr[9:4]},
+	DCACHE_state_trace (addr, dout, {63'b0,valid}, {63'b0,ready}, line_mem_prev[127:64], line_mem_prev[63:0], {60'b0,index}, {58'b0,addr[9:4]},
 	{60'b0,addr[3:0]}, {60'b0,addr[3:0]}, {60'b0,way_hit}, {60'b0,way_hit_prev}, {60'b0,cen3,cen2,cen1,cen0}, {63'b0,wen}, line_mem[127:64], line_mem[63:0]);
 end
 //*****************************************************************
@@ -44,45 +51,45 @@ end
 integer i;
 always @(posedge clk) begin
 	if(rst) begin
-		v0 <= 64'b0;
-		v1 <= 64'b0;
-		v2 <= 64'b0;
-		v3 <= 64'b0;
-		for(i=0;i<=63;i=i+1)begin
-			tag0[i] <= 54'b0;
-			tag1[i] <= 54'b0;
-			tag2[i] <= 54'b0;
-			tag3[i] <= 54'b0;
+		v0 <= 16'b0;
+		v1 <= 16'b0;
+		v2 <= 16'b0;
+		v3 <= 16'b0;
+		for(i=0;i<=15;i=i+1)begin
+			tag0[i] <= 22'b0;
+			tag1[i] <= 22'b0;
+			tag2[i] <= 22'b0;
+			tag3[i] <= 22'b0;
 		end
 	end
-	else if( (!cen0|!cen1|!cen2|!cen3)&&!wen  ) begin
+	else if( !wen && rlast  ) begin
 	    if(not_device) begin   //暂时用来维持设备和dcache的一致性
 		case({!cen3,!cen2,!cen1,!cen0})
-			4'b0001: begin v0[index] <= 1'b1; tag0[index] <= addr[63:10]; end 
-			4'b0010: begin v1[index] <= 1'b1; tag1[index] <= addr[63:10]; end
-			4'b0100: begin v2[index] <= 1'b1; tag2[index] <= addr[63:10]; end
-			4'b1000: begin v3[index] <= 1'b1; tag3[index] <= addr[63:10]; end
-			default: begin $display("icache all misses!!!!!!!!!!!!!!!!!!!!!!!!\n\n");end
+			4'b0001: begin v0[index] <= 1'b1; tag0[index] <= addr[31:10]; end 
+			4'b0010: begin v1[index] <= 1'b1; tag1[index] <= addr[31:10]; end
+			4'b0100: begin v2[index] <= 1'b1; tag2[index] <= addr[31:10]; end
+			4'b1000: begin v3[index] <= 1'b1; tag3[index] <= addr[31:10]; end
+			default: begin $display("dcache all misses!!!!!!!!!!!!!!!!!!!!!!!!\n\n");end
 		endcase
 	    end
 	end
-/*
-	if(waddr!=0) begin
-		if(tag0[waddr[9:4]] == waddr[63:10]) begin v0[waddr[9:4]] <= 1'b0; end
-		if(tag1[waddr[9:4]] == waddr[63:10]) begin v1[waddr[9:4]] <= 1'b0; end
-		if(tag2[waddr[9:4]] == waddr[63:10]) begin v2[waddr[9:4]] <= 1'b0; end
-		if(tag3[waddr[9:4]] == waddr[63:10]) begin v3[waddr[9:4]] <= 1'b0; end
+
+	if(addr!=0 && wren) begin
+		if(tag0[addr[9:6]] == addr[31:10]) begin v0[addr[9:6]] <= 1'b0; end
+		if(tag1[addr[9:6]] == addr[31:10]) begin v1[addr[9:6]] <= 1'b0; end
+		if(tag2[addr[9:6]] == addr[31:10]) begin v2[addr[9:6]] <= 1'b0; end
+		if(tag3[addr[9:6]] == addr[31:10]) begin v3[addr[9:6]] <= 1'b0; end
 	end
-*/
+
 end
 
-wire [5:0]index;
-assign index = addr[9:4];
+wire [3:0]index;
+assign index = addr[9:6];
 wire [3:0]way_hit;
-assign way_hit[0] = v0[index] && (tag0[index] == addr[63:10]);
-assign way_hit[1] = v1[index] && (tag1[index] == addr[63:10]);
-assign way_hit[2] = v2[index] && (tag2[index] == addr[63:10]);
-assign way_hit[3] = v3[index] && (tag3[index] == addr[63:10]);
+assign way_hit[0] = v0[index] && (tag0[index] == addr[31:10]);
+assign way_hit[1] = v1[index] && (tag1[index] == addr[31:10]);
+assign way_hit[2] = v2[index] && (tag2[index] == addr[31:10]);
+assign way_hit[3] = v3[index] && (tag3[index] == addr[31:10]);
 
 wire not_device;
 assign not_device = (addr <= 64'h8fffffff);    //暂时用来调整一致性
@@ -94,15 +101,50 @@ wire [127:0]bwen;
 wire [5:0]addr_sram;
 wire [127:0]din_sram;
 
-assign addr_sram = index;
-assign cen0 = ~( (valid&&!ready && not_device) ? (way_hit[0] ? 1'b1 : (way_hit==4'b0&&random_cnt[0] ? 1'b1 : 1'b0)) : 1'b0) ;
-assign cen1 = ~( (valid&&!ready && not_device) ? (way_hit[1] ? 1'b1 : (way_hit==4'b0&&random_cnt[1] ? 1'b1 : 1'b0)) : 1'b0) ;
-assign cen2 = ~( (valid&&!ready && not_device) ? (way_hit[2] ? 1'b1 : (way_hit==4'b0&&random_cnt[2] ? 1'b1 : 1'b0)) : 1'b0) ;
-assign cen3 = ~( (valid&&!ready && not_device) ? (way_hit[3] ? 1'b1 : (way_hit==4'b0&&random_cnt[3] ? 1'b1 : 1'b0)) : 1'b0) ;
-assign  wen = ~( (valid&&!ready && not_device) && (wren || ((!wren)&&(way_hit == 4'b0))) )  ;
-assign  din_sram = wren ? (way_hit==4'b0 ? line_mem_wr:(addr[3] ? {din,64'b0}:{64'b0,din})) : line_mem;
-assign bwen = wren ? (way_hit==4'b0 ? 128'b0 : (addr[3] ? (~{mask,64'b0}):(~{64'b0,mask}))) : 128'b0;
 
+reg [3:0]wr_sram_count;
+reg [3:0]random_cnt;
+
+always @(posedge clk) begin
+	if(rst) begin
+		wr_sram_count      <= 4'b0;
+		random_cnt         <= 4'b1;
+	end
+	else if(dcache_current_state==idle) begin
+		wr_sram_count      <= 4'b0;
+		random_cnt[0]      <= random_cnt[3];
+		random_cnt[3:1]    <= random_cnt[2:0];
+	end
+	else begin
+		wr_sram_count      <= wr_sram_count+4'b1;
+		random_cnt         <= random_cnt;
+	end
+end
+
+
+
+assign        addr_sram =  addr[9:4];
+assign     bwen[31 :0 ] = (wr_sram_count[1:0]==2'b00) ? 32'b0 : 32'hffffffff; 
+assign     bwen[63 :32] = (wr_sram_count[1:0]==2'b01) ? 32'b0 : 32'hffffffff; 
+assign     bwen[95 :64] = (wr_sram_count[1:0]==2'b10) ? 32'b0 : 32'hffffffff; 
+assign     bwen[127:96] = (wr_sram_count[1:0]==2'b11) ? 32'b0 : 32'hffffffff; 
+assign din_sram[31 :0 ] = (wr_sram_count[1:0]==2'b00) ? rdata : 32'b0; 
+assign din_sram[63 :32] = (wr_sram_count[1:0]==2'b01) ? rdata : 32'b0; 
+assign din_sram[95 :64] = (wr_sram_count[1:0]==2'b10) ? rdata : 32'b0; 
+assign din_sram[127:96] = (wr_sram_count[1:0]==2'b11) ? rdata : 32'b0; 
+assign cen0 = ~(  (dcache_current_state==idle) ? (valid&&way_hit[0]) : (random_cnt[0]&&rvalid&&rready)      ) ;
+assign cen1 = ~(  (dcache_current_state==idle) ? (valid&&way_hit[1]) : (random_cnt[1]&&rvalid&&rready)      ) ;
+assign cen2 = ~(  (dcache_current_state==idle) ? (valid&&way_hit[2]) : (random_cnt[2]&&rvalid&&rready)      ) ;
+assign cen3 = ~(  (dcache_current_state==idle) ? (valid&&way_hit[3]) : (random_cnt[3]&&rvalid&&rready)      ) ;
+assign  wen = ~(   dcache_current_state==readmemory && rvalid && rready            ) ;
+//assign addr_sram = index;
+//assign cen0 = ~( (valid&&!ready && not_device) ? (way_hit[0] ? 1'b1 : (way_hit==4'b0&&random_cnt[0] ? 1'b1 : 1'b0)) : 1'b0) ;
+//assign cen1 = ~( (valid&&!ready && not_device) ? (way_hit[1] ? 1'b1 : (way_hit==4'b0&&random_cnt[1] ? 1'b1 : 1'b0)) : 1'b0) ;
+//assign cen2 = ~( (valid&&!ready && not_device) ? (way_hit[2] ? 1'b1 : (way_hit==4'b0&&random_cnt[2] ? 1'b1 : 1'b0)) : 1'b0) ;
+//assign cen3 = ~( (valid&&!ready && not_device) ? (way_hit[3] ? 1'b1 : (way_hit==4'b0&&random_cnt[3] ? 1'b1 : 1'b0)) : 1'b0) ;
+//assign  wen = ~( (valid&&!ready && not_device) && (wren || ((!wren)&&(way_hit == 4'b0))) )  ;
+//assign  din_sram = wren ? (way_hit==4'b0 ? line_mem_wr:(addr[3] ? {din,64'b0}:{64'b0,din})) : line_mem;
+//assign bwen = wren ? (way_hit==4'b0 ? 128'b0 : (addr[3] ? (~{mask,64'b0}):(~{64'b0,mask}))) : 128'b0;
 S011HD1P_X32Y2D128_BW sram_d0(dout0, clk, cen0, wen, bwen, addr_sram, din_sram);
 S011HD1P_X32Y2D128_BW sram_d1(dout1, clk, cen1, wen, bwen, addr_sram, din_sram);
 S011HD1P_X32Y2D128_BW sram_d2(dout2, clk, cen2, wen, bwen, addr_sram, din_sram);
@@ -110,12 +152,10 @@ S011HD1P_X32Y2D128_BW sram_d3(dout3, clk, cen3, wen, bwen, addr_sram, din_sram);
 
 
 reg [3:0]way_hit_prev;
-reg [3:0]random_cnt;
 reg [127:0]line_mem_prev;
 always @(posedge clk) begin
 	if(rst) begin
 		way_hit_prev    <= 4'b0;
-		random_cnt      <= 4'b1;
 		line_mem_prev   <=128'b0;
 		ready           <= 1'b0;
 	end
@@ -127,27 +167,36 @@ always @(posedge clk) begin
 		ready           <= ready        ;
 	end
 	*/
-       /*
-	else if(flush) begin
+	else if(valid && wren && dcache_current_state==idle)begin
+	     	way_hit_prev    <= 4'b0;
+		line_mem_prev   <= line_mem;
+		ready           <= 1'b1;
+	end
+	else if(valid && way_hit!=4'b0 && !wren && dcache_current_state==idle)begin
+	     	way_hit_prev    <= way_hit;
+		line_mem_prev   <= line_mem;
+		ready           <= 1'b1;
+	end
+	else begin
 		way_hit_prev    <= 4'b0;
-		line_mem_prev   <=128'b0;
+		line_mem_prev   <= line_mem;
 		ready           <= 1'b0;
 	end
-	*/
+
+
+
+       /*
 	else if(ready && valid) begin
 	     	way_hit_prev    <= way_hit;
-		random_cnt[0]   <= random_cnt[3];
-		random_cnt[3:1] <= random_cnt[2:0];
 		line_mem_prev   <= line_mem;
 		ready           <= 1'b0;
 	end
 	else if(!ready && valid) begin
 	     	way_hit_prev    <= way_hit;
-		random_cnt[0]   <= random_cnt[3];
-		random_cnt[3:1] <= random_cnt[2:0];
 		line_mem_prev   <= line_mem;
 		ready           <= 1'b1;
 	end
+	*/
 end
 
 reg [127:0]line_read;
@@ -157,7 +206,8 @@ always @(*) begin
 		4'b0010: line_read = dout1;
 		4'b0100: line_read = dout2;
 		4'b1000: line_read = dout3;
-		default: line_read = line_mem_prev;
+		default: line_read = 128'b0;
+		//default: line_read = line_mem_prev;
 	endcase
 end
 
@@ -165,8 +215,75 @@ end
 
 assign dout =  addr[3] ?  line_read[127:64] : line_read[63:0] ;
 
-//wire [127:0]wr_miss_line;
-//assign wr_miss_line = 
+
+//*******************   AXI-FULL    ***********************
+wire [31:0]araddr;
+wire [7:0]arlen;
+wire [2:0]arsize;
+wire [1:0]arburst;
+reg arvalid;
+wire arready;
+
+wire [31:0]rdata;
+wire [1:0]rrsep;
+wire rlast;
+wire rvalid;
+wire rready;
+
+
+ysyx_22050612_SRAM  sram_mem (clk, rst, araddr, arlen, arsize, arburst, arvalid, arready,    rdata, rrsep, rlast, rvalid, rready);
+
+assign araddr  = {addr[31:6],6'b0};
+assign arlen   = 8'b11;                                    //The real length is arlen + 1
+//assign arlen   = 8'b1111;                                    //The real length is arlen + 1
+assign arsize  = 3'b101;
+assign arburst = 2'b01;
+
+assign rready  = 1'b1;
+
+
+//****************     dcahce state machine   ***************
+reg [1:0]dcache_current_state, dcache_next_state;
+
+localparam idle       = 2'b00;        //
+localparam readmemory = 2'b01;        //
+
+
+always @(posedge clk) begin
+	if(rst == 1'b1) dcache_current_state <= idle;
+	else            dcache_current_state <= dcache_next_state;
+end
+
+always @(*) begin
+	case(dcache_current_state)
+		idle: begin
+			arvalid = valid && (way_hit==4'b0) && !wren;
+			dcache_next_state = (valid && (way_hit==4'b0) && !wren && arready) ? readmemory : idle;
+		end
+		readmemory: begin
+			arvalid = 1'b0;
+			dcache_next_state = rlast ? idle : readmemory;
+		end
+		default: begin
+			arvalid = 1'b0;
+			dcache_next_state = idle;
+		end
+	endcase
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 wire [127:0]line_mem;
--- npc/vsrc/ysyx_22050612_SRAM.v
+++ npc/vsrc/ysyx_22050612_SRAM.v
@@ -26,13 +26,17 @@ module ysyx_22050612_SRAM(
    input  rready
 /*
 //write
-   input awvalid,
    input [31:0]awaddr,
+   input [7:0]awlen,
+   input [2:0]awsize,
+   input [1:0]arburst,
+   input awvalid,
    output awready,
 
-   input wvalid,
    input [63:0]wdata,
    input [ 7:0]wstrb,
+   input wlast,
+   input wvalid,
    output wready,
 
    output reg [1:0]bresp,
@@ -106,120 +110,81 @@ always @(read_current_state or arvalid) begin
 		end
 	endcase
 end
-/*
-//reg delay_read;
 
-always @(posedge clk) begin
-	//$display("sram:   arvalid = %d  arready = %d  \n",arvalid, arready);   
-	if(rst == 1'b1)begin
-		rvalid <= 1'b0;
-		rresp <= 2'b0;
-	//	delay_read <=1'b0;
-	end
-//	else if(arready == 1'b1 && arvalid == 1'b1)begin
-//		delay_read <=1'b1;
-//	end
-//	else if(delay_read ==1'b1)begin
-	else if(arready == 1'b1 && arvalid == 1'b1)begin
-	//	delay_read <=1'b0;
-		rvalid <= 1'b1;
-  		pmem_read({{32{1'b0}},araddr}, rdata);	
-  		//pmem_read_pc({{32{1'b0}},araddr}, rdata);	
-		//$display("get inst!!  %x  %x\n",araddr,rdata);
-		//$display("2\n");
-		rresp <= 2'b0;
-	end
-	else if(rvalid == 1'b1 && rready == 1'b1)begin
-		rvalid <= 1'b0;
-		rresp <= 2'b0;
-		//$display("4\n");
-	end
-end
-*/
 
 
 
 /*
-reg aaa;
-always @(read_current_state) begin
-	if(rst) aaa = 1'b0;
-	else aaa = read_current_state[0];
-	$display("clk:%d  state:%d  aaa:%d",clk,read_current_state,aaa);
-end
-*/
-
-
-
 //************** write *******************
-//assign awready = 1'b1;
-//assign wready  = 1'b1;
-//reg [1:0]write_current_state, write_next_state;
-//
-//localparam write_idle  = 2'b00;
-//localparam write_aw_hs = 2'b01;        //write address handshake
-//localparam write_w_rsp = 2'b11;        //write respone
-//
-//always @(posedge clk) begin
-//	if(rst == 1'b1) write_current_state <= write_idle;
-//	else            write_current_state <= write_next_state;
-//end
-//
-//always @(*) begin
-//	case(write_current_state)
-//		write_idle: begin
-//			bvalid = 1'b0;
-//			bresp  = 2'b0;
-//			write_next_state = (awvalid && wvalid)? write_aw_hs:write_idle;
-//		end
-//		write_aw_hs: begin
-//			pmem_write({{32{1'b0}},awaddr}, wdata, wstrb);
-//			bvalid = 1'b1;
-//			bresp  = 2'b0;
-//			write_next_state = write_w_rsp;
-//		end
-//		write_w_rsp: begin
-//			bvalid = 1'b0;
-//			bresp  = 2'b0;
-//			write_next_state = write_idle;
-//		end
-//		default: begin
-//			bvalid = 1'b0;
-//			bresp  = 2'b0;
-//			write_next_state = write_idle;
-//		end
-//	endcase
-//end
-/*
-//reg delay_write;
+reg [1:0]write_current_state, write_next_state;
+
+localparam write_idle  = 2'b00;
+localparam write_receive_wdata = 2'b01;        //receive wdata and write it to memory
+localparam write_w_rsp = 2'b11;                //write respone
+
+reg [31:0]w_addr;
+reg  [7:0]w_len;
+reg  [2:0]w_size;
+reg  [1:0]w_burst;
+reg  [7:0]w_count;
 
 always @(posedge clk) begin
-	//$display("sram:   arvalid = %d  arready = %d  \n",arvalid, arready);   
-	if(rst == 1'b1)begin
-		bvalid <= 1'b0;
-		bresp <= 2'b0;
-	//	delay_write <= 1'b0;
+	if(rst == 1'b1) write_current_state <= write_idle;
+	else            write_current_state <= write_next_state;
+
+	if(awvalid&&awready&&(write_current_state==write_idle)) begin
+		w_addr  <= awaddr;
+		w_len   <= awlen;
+		w_size  <= awsize;
+		w_burst <= awburst;
+
+		w_count <= 8'b0;
 	end
-//	else if(awready == 1'b1 && awvalid == 1'b1 && wready == 1'b1 && wvalid == 1'b1)begin
-//		delay_write <= 1'b1;
-//	end
-//	else if(delay_write == 1'b1)begin
-	else if(awready == 1'b1 && awvalid == 1'b1 && wready == 1'b1 && wvalid == 1'b1)begin
-	//	delay_write <= 1'b0;
-		pmem_write({{32{1'b0}},awaddr}, wdata, wstrb);
-		//$display("write!!  %x  %x\n",awaddr,wdata);
-		//$display("2\n");
-		bresp <= 2'b00;
-		bvalid <= 1'b1;
+	else if(write_current_state == write_receive_wdata) begin
+		w_count <= w_count + 8'h1;
 	end
-	else if(bvalid == 1'b1 && bready == 1'b1)begin
-		bvalid <= 1'b0;
-		//$display("write  respon!! \n");
-		//bresp <= 1'b1;
-		//$display("4\n");
-	end
+end
+
+
+always @(posedge clk) begin
+	if(rst == 1'b1) write_current_state <= write_idle;
+	else            write_current_state <= write_next_state;
+end
+
+always @(*) begin
+	case(write_current_state)
+		write_idle: begin
+			awready = 1'b1;
+			wready  = 1'b0;
+			bvalid = 1'b0;
+			bresp  = 2'b0;
+			write_next_state = (awvalid && awready) ? write_receive_wdata : write_idle;
+		end
+		write_receive_wdata: begin
+			pmem_write({{32{1'b0}},w_addr}, wdata, wstrb);
+			awready = 1'b0;
+			wready  = 1'b1;
+			bvalid = 1'b0;
+			bresp  = 2'b0;
+			write_next_state = (w_count == w_len) ? write_w_rsp : write_receive_wdata;
+		end
+		write_w_rsp: begin
+			awready = 1'b0;
+			wready  = 1'b0;
+			bvalid = 1'b1;
+			bresp  = 2'b0;
+			write_next_state = write_idle;
+		end
+		default: begin
+			awready = 1'b1;
+			wready  = 1'b0;
+			bvalid = 1'b0;
+			bresp  = 2'b0;
+			write_next_state = write_idle;
+		end
+	endcase
 end
 */
 
 
 endmodule
-
