dcbe4e3ea63355918e0c992c42600a8ae18652cb
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-01 10:40:15 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:15 up 57 min,  8 users,  load average: 0.09, 0.04, 0.19

--- npc/vsrc/ysyx_22050612_Arbiter.v
+++ npc/vsrc/ysyx_22050612_Arbiter.v
@@ -1,273 +1,244 @@
 
-module ysyx_22050612_Arbiter(
+module ysyx_mem2050612_Arbiter(
    input clk,
    input rst,
-   
-   input reg arvalid_1,
-   input reg arvalid_2,
-   output reg arvalid,
-   input [31:0]araddr_1,
-   input [31:0]araddr_2,
+
+//read address channel 
+   input  [31:0]araddr_ifu,
+   input  [31:0]araddr_mem,
    output [31:0]araddr,
-   output arready_1,
-   output arready_2,
-   input arready,
+   input  [ 7:0]arlen_ifu,
+   input  [ 7:0]arlen_mem,
+   output [ 7:0]arlen,
+   input  [ 2:0]arsize_ifu,
+   input  [ 2:0]arsize_mem,
+   output [ 2:0]arsize,
+   input  [ 1:0]arburst_ifu,
+   input  [ 1:0]arburst_mem,
+   output [ 1:0]arburst,
+   input        arvalid_ifu,
+   input        arvalid_mem,
+   output       arvalid,
+   input        arready,
+   output       arready_ifu,
+   output       arready_mem,
 
-   output reg rvalid_1,
-   output reg rvalid_2,
-   input reg rvalid,
-   output [63:0]rdata_1,
-   output [63:0]rdata_2,
-   input [63:0]rdata,
-   output reg [1:0]rresp_1,
-   output reg [1:0]rresp_2,
-   input reg [1:0]rresp,
-   input rready_1,
-   input rready_2,
-   output rready,
+//read data channel
+   input  [63:0]rdata,
+   output [63:0]rdata_ifu,
+   output [63:0]rdata_mem,
+   input  [ 1:0]rresp,
+   output [ 1:0]rresp_ifu,
+   output [ 1:0]rresp_mem,
+   input        rlast,
+   output       rlast_ifu,
+   output       rlast_mem,
+   input        rvalid,
+   output       rvalid_ifu,
+   output       rvalid_mem,
+   input        rready_ifu,
+   input        rready_mem,
+   output       rready,
 
-   input awvalid_1,
-   input awvalid_2,
-   output awvalid,
-   input [31:0]awaddr_1,
-   input [31:0]awaddr_2,
+
+//write address channel
+   input  [31:0]awaddr_ifu,
+   input  [31:0]awaddr_mem,
    output [31:0]awaddr,
-   output awready_1,
-   output awready_2,
-   input awready,
+   input  [ 7:0]awlen_ifu,
+   input  [ 7:0]awlen_mem,
+   output [ 7:0]awlen,
+   input  [ 2:0]awsize_ifu,
+   input  [ 2:0]awsize_mem,
+   output [ 2:0]awsize,
+   input  [ 1:0]awburst_ifu,
+   input  [ 1:0]awburst_mem,
+   output [ 1:0]awburst,
+   input        awvalid_ifu,
+   input        awvalid_mem,
+   output       awvalid,
+   input        awready,
+   output       awready_ifu,
+   output       awready_mem,
 
-   input wvalid_1,
-   input wvalid_2,
-   output wvalid,
-   input [63:0]wdata_1,
-   input [63:0]wdata_2,
+
+
+//write data channel
+   input  [63:0]wdata_ifu,
+   input  [63:0]wdata_mem,
    output [63:0]wdata,
-   input [ 7:0]wstrb_1,
-   input [ 7:0]wstrb_2,
-   output [ 7:0]wstrb,
-   output wready_1,
-   output wready_2,
-   input wready,
+   input  [ 1:0]wstrb_ifu,
+   input  [ 1:0]wstrb_mem,
+   output [ 1:0]wstrb,
+   input        wlast_ifu,
+   input        wlast_mem,
+   output       wlast,
+   input        wvalid_ifu,
+   input        wvalid_mem,
+   output       wvalid,
+   input        wready,
+   output       wready_ifu,
+   output       wready_mem,
 
-   output [1:0]bresp_1,
-   output [1:0]bresp_2,
+
+
+//write respond channel
    input [1:0]bresp,
-   output bvalid_1,
-   output bvalid_2,
+   output [1:0]bresp_ifu,
+   output [1:0]bresp_mem,
    input bvalid,
-   input bready_1,
-   input bready_2,
+   output bvalid_ifu,
+   output bvalid_mem,
+   input bready_ifu,
+   input bready_mem,
    output bready
 
 
 );
 
 
-//*********************    write    ********************
-reg writing_1;
-reg writing_2;
-wire writing;
-assign writing = writing_1 || writing_2;
-
-always @(*) begin
-	if(writing == 1'b0 && (awvalid_1 == 1'b1 || wvalid_1 == 1'b1)) begin
-		awvalid   = awvalid_1;
-		awaddr    = awaddr_1;
-		awready_1 = awready;
-		awready_2 = 1'b0; 
-		wvalid    = wvalid_1;
-		wdata     = wdata_1;
-		wstrb     = wstrb_1;
-		wready_1  = wready;
-		wready_2  = 1'b0; 
-		bresp_1   = bresp;
-		bresp_2   = 2'b0; 
-		bvalid_1  = bvalid;
-		bvalid_2  = 1'b0; 
-		bready    = bready_1;
-	end
-	else if(writing == 1'b0 && (awvalid_2 == 1'b1 || wvalid_2 == 1'b1)) begin
-		awvalid   = awvalid_2;
-		awaddr    = awaddr_2;
-		awready_1 = 1'b0; 
-		awready_2 = awready;
-		wvalid    = wvalid_2;
-		wdata     = wdata_2;
-		wstrb     = wstrb_2;
-		wready_1  = 1'b0; 
-		wready_2  = wready;
-		bresp_1   = 2'b0; 
-		bresp_2   = bresp;
-		bvalid_1  = 1'b0; 
-		bvalid_2  = bvalid;
-		bready    = bready_2;
-	end
-	else if(writing_1 == 1'b1) begin
-		awvalid   = awvalid_1;
-		awaddr    = awaddr_1;
-		awready_1 = awready;
-		awready_2 = 1'b0; 
-		wvalid    = wvalid_1;
-		wdata     = wdata_1;
-		wstrb     = wstrb_1;
-		wready_1  = wready;
-		wready_2  = 1'b0; 
-		bresp_1   = bresp;
-		bresp_2   = 2'b0; 
-		bvalid_1  = bvalid;
-		bvalid_2  = 1'b0; 
-		bready    = bready_1;
-	end
-	else if(writing_2 == 1'b1) begin
-		awvalid   = awvalid_2;
-		awaddr    = awaddr_2;
-		awready_1 = 1'b0; 
-		awready_2 = awready;
-		wvalid    = wvalid_2;
-		wdata     = wdata_2;
-		wstrb     = wstrb_2;
-		wready_1  = 1'b0; 
-		wready_2  = wready;
-		bresp_1   = 2'b0; 
-		bresp_2   = bresp;
-		bvalid_1  = 1'b0; 
-		bvalid_2  = bvalid;
-		bready    = bready_2;
-	end
-	else begin
-		awvalid = 1'b0; 
-		awaddr  = 32'b0; 
-		awready_1 = 1'b0; 
-		awready_2 = 1'b0; 
-		wvalid  = 1'b0; 
-		wdata   = 64'b0; 
-		wstrb   = 8'b0; 
-		wready_1  = 1'b0; 
-		wready_2  = 1'b0; 
-		bresp_1   = 2'b0; 
-		bresp_2   = 2'b0; 
-		bvalid_1  = 1'b0; 
-		bvalid_2  = 1'b0; 
-		bready  = 1'b0; 
-	end
-end
-
-always @(posedge clk) begin
-	if(rst == 1'b1)begin
-		writing_1 <=1'b0;
-		writing_2 <=1'b0;
-	end
-	else if(writing == 1'b0 && awvalid_1 == 1'b1) begin
-		writing_1 <=1'b1;
-	end
-	else if(writing == 1'b0 && awvalid_2 == 1'b1) begin
-		writing_2 <=1'b1;
-	end
-	else if(writing_1 == 1'b1 && bvalid_1 == 1'b1 && bready_1 == 1'b1) begin
-		writing_1 <=1'b0;
-	end
-	else if(writing_2 == 1'b1 && bvalid_2 == 1'b1 && bready_2 == 1'b1) begin
-		writing_2 <=1'b0;
-	end
-end
-
 
 
 //*********************    read    ********************
-reg reading_1;
-reg reading_2;
-wire reading;
-assign reading = reading_1 || reading_2;
+reg arbi_r_state, arbi_r_next_state;
+
+localparam r_idle        = 1'b0;
+localparam r_transforming = 1'b1;
+
+always @(posedge clk) begin
+	if(rst)  arbi_r_state <= r_idle;
+	else     arbi_r_state <= arbi_r_next_state;
+end
 
 always @(*) begin
-	if(reading == 1'b0 && arvalid_1 == 1'b1) begin
-		arvalid   = arvalid_1;
-		araddr    = araddr_1;
-		arready_1 = arready;
-		arready_2 = 1'b0; 
-		rvalid_1  = rvalid;
-		rvalid_2  = 1'b0; 
-		rdata_1   = rdata;
-		rdata_2   = 64'b0; 
-		rresp_1   = rresp;
-		rresp_2   = 2'b0; 
-		rready    = rready_1;
+	case(arbi_r_state)
+		r_idle: begin
+		end
+		r_transforming: begin
+		end
+		idle: begin
+		end
+
+end
+
+
+
+/*
+reg reading_ifu;
+reg reading_mem;
+wire reading;
+assign reading = reading_ifu || reading_mem;
+
+always @(*) begin
+	if(reading == 1'b0 && arvalid_ifu == 1'b1) begin
+		arvalid   = arvalid_ifu;
+		araddr    = araddr_ifu;
+		arready_ifu = arready;
+		arready_mem = 1'b0; 
+		rvalid_ifu  = rvalid;
+		rvalid_mem  = 1'b0; 
+		rdata_ifu   = rdata;
+		rdata_mem   = 64'b0; 
+		rresp_ifu   = rresp;
+		rresp_mem   = 2'b0; 
+		rready    = rready_ifu;
 	end
-	else if(reading == 1'b0 && arvalid_2 == 1'b1) begin
-		arvalid   = arvalid_2;
-		araddr    = araddr_2;
-		arready_1 = 1'b0; 
-		arready_2 = arready;
-		rvalid_1  = 1'b0; 
-		rvalid_2  = rvalid;
-		rdata_1   = 64'b0; 
-		rdata_2   = rdata;
-		rresp_1   = 2'b0; 
-		rresp_2   = rresp;
-		rready    = rready_2;
+	else if(reading == 1'b0 && arvalid_mem == 1'b1) begin
+		arvalid   = arvalid_mem;
+		araddr    = araddr_mem;
+		arready_ifu = 1'b0; 
+		arready_mem = arready;
+		rvalid_ifu  = 1'b0; 
+		rvalid_mem  = rvalid;
+		rdata_ifu   = 64'b0; 
+		rdata_mem   = rdata;
+		rresp_ifu   = 2'b0; 
+		rresp_mem   = rresp;
+		rready    = rready_mem;
 	end
-	else if(reading_1 == 1'b1) begin
-		arvalid   = arvalid_1;
-		araddr    = araddr_1;
-		arready_1 = arready;
-		arready_2 = 1'b0; 
-		rvalid_1  = rvalid;
-		rvalid_2  = 1'b0; 
-		rdata_1   = rdata;
-		rdata_2   = 64'b0; 
-		rresp_1   = rresp;
-		rresp_2   = 2'b0; 
-		rready    = rready_1;
+	else if(reading_ifu == 1'b1) begin
+		arvalid   = arvalid_ifu;
+		araddr    = araddr_ifu;
+		arready_ifu = arready;
+		arready_mem = 1'b0; 
+		rvalid_ifu  = rvalid;
+		rvalid_mem  = 1'b0; 
+		rdata_ifu   = rdata;
+		rdata_mem   = 64'b0; 
+		rresp_ifu   = rresp;
+		rresp_mem   = 2'b0; 
+		rready    = rready_ifu;
 	end
-	else if(reading_2 == 1'b1) begin
-		arvalid   = arvalid_2;
-		araddr    = araddr_2;
-		arready_1 = 1'b0; 
-		arready_2 = arready;
-		rvalid_1  = 1'b0; 
-		rvalid_2  = rvalid;
-		rdata_1   = 64'b0; 
-		rdata_2   = rdata;
-		rresp_1   = 2'b0; 
-		rresp_2   = rresp;
-		rready    = rready_2;
+	else if(reading_mem == 1'b1) begin
+		arvalid   = arvalid_mem;
+		araddr    = araddr_mem;
+		arready_ifu = 1'b0; 
+		arready_mem = arready;
+		rvalid_ifu  = 1'b0; 
+		rvalid_mem  = rvalid;
+		rdata_ifu   = 64'b0; 
+		rdata_mem   = rdata;
+		rresp_ifu   = 2'b0; 
+		rresp_mem   = rresp;
+		rready    = rready_mem;
 	end
 	else  begin
 		arvalid   = 1'b0; 
 		araddr    = 32'b0; 
-		arready_1 = 1'b0; 
-		arready_2 = 1'b0; 
-		rvalid_1  = 1'b0; 
-		rvalid_2  = 1'b0; 
-		rdata_1   = 64'b0; 
-		rdata_2   = 64'b0; 
-		rresp_1   = 2'b0; 
-		rresp_2   = 2'b0; 
+		arready_ifu = 1'b0; 
+		arready_mem = 1'b0; 
+		rvalid_ifu  = 1'b0; 
+		rvalid_mem  = 1'b0; 
+		rdata_ifu   = 64'b0; 
+		rdata_mem   = 64'b0; 
+		rresp_ifu   = 2'b0; 
+		rresp_mem   = 2'b0; 
 		rready    = 1'b0; 
 	end
 end
 
 always @(posedge clk) begin
 	if(rst == 1'b1)begin
-		reading_1 <=1'b0;
-		reading_2 <=1'b0;
+		reading_ifu <=1'b0;
+		reading_mem <=1'b0;
 	end
-	else if(reading == 1'b0 && arvalid_1 == 1'b1) begin
-		reading_1 <=1'b1;
+	else if(reading == 1'b0 && arvalid_ifu == 1'b1) begin
+		reading_ifu <=1'b1;
 	end
-	else if(reading == 1'b0 && arvalid_2 == 1'b1) begin
-		reading_2 <=1'b1;
+	else if(reading == 1'b0 && arvalid_mem == 1'b1) begin
+		reading_mem <=1'b1;
 	end
-	else if(reading_1 == 1'b1 && rvalid_1 == 1'b1 && rready_1 == 1'b1) begin
-		reading_1 <=1'b0;
+	else if(reading_ifu == 1'b1 && rvalid_ifu == 1'b1 && rready_ifu == 1'b1) begin
+		reading_ifu <=1'b0;
 	end
-	else if(reading_2 == 1'b1 && rvalid_2 == 1'b1 && rready_2 == 1'b1) begin
-		reading_2 <=1'b0;
+	else if(reading_mem == 1'b1 && rvalid_mem == 1'b1 && rready_mem == 1'b1) begin
+		reading_mem <=1'b0;
 	end
 end
+*/
 
 
 
+//*********************    write    ********************
+assign awaddr      = awaddr_mem   ;
+assign awlen       = awlen_mem    ;
+assign awsize      = awsize_mem   ;
+assign awburst     = awburst_mem  ;
+assign awvalid     = awvalid_mem  ;
+assign awready_ifu = 1'b0       ;
+assign awready_mem = awready    ;
 
+assign wdata       = wdata_mem    ;
+assign wstrb       = wstrb_mem    ;
+assign wlast       = wlast_mem    ;
+assign wvalid      = wvalid_mem   ;
+assign wready_ifu  = 1'b0       ;
+assign wready_mem  = wready     ;
+
+assign bresp_ifu   = 2'b0       ;
+assign bresp_mem   = bresp      ;
+assign bvalid_ifu  = 1'b0       ;
+assign bvalid_mem  = bvalid     ;
+assign bready      = bready_mem   ;
 
 endmodule
