54713a64d8c72ca9aa3f2785122b01d76b48283c
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 10:58:24 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:24 up  8:33,  8 users,  load average: 0.34, 0.27, 0.28

--- npc/vsrc/ysyx_22050612_multiplier.v
+++ npc/vsrc/ysyx_22050612_multiplier.v
@@ -14,7 +14,6 @@ module ysyx_22050612_multiplier(
 );
 
 always @(negedge clk)begin
-	$display("mulresult_inaaaaaaaaaaaaa");
 	$display("mulresult_inaaaaaaaaaaaaa:%d  %d",result_hi,result_lo);
 end
 
@@ -70,6 +69,8 @@ ysyx_22050612_PART_PRODUCT  part_product32 (mul_a[65:63],{{ 2{1'b0}},mul_b,{64{1
 
 wire [32:0] walloc_din [131:0];
 wire [30:0] walloc_cgroup [131:1];
+wire [30:0] walloc_cgroup1[131:1];
+wire [30:0] walloc_cgroup2[131:1];
 wire [130:0] walloc_c;
 wire [131:0] walloc_s;
 genvar i;
@@ -81,12 +82,14 @@ endgenerate
 
 
 ysyx_22050612_WALLOC_33BITS walloc_tree0   (walloc_din[0],c[30:0],walloc_cgroup[1],walloc_c[0],walloc_s[0] );
+ysyx_22050612_WALLOC_33BITS walloc_tree1   (walloc_din[1],walloc_cgroup1[1],walloc_cgroup2[1],walloc_c[1],walloc_s[1] );
+ysyx_22050612_WALLOC_33BITS walloc_tree2   (walloc_din[2],walloc_cgroup2[1],,walloc_c[2],walloc_s[2] );
 ysyx_22050612_WALLOC_33BITS walloc_tree131 (walloc_din[131],walloc_cgroup[131], , ,walloc_s[131] );
 
 genvar j;
 generate
 	for(j=1;j<131;j=j+1) begin: walloc_gen
-		ysyx_22050612_WALLOC_33BITS walloc_tree  (walloc_din[j],walloc_cgroup[j],walloc_cgroup[j+1],walloc_c[j],walloc_s[j] );
+		//ysyx_22050612_WALLOC_33BITS walloc_tree  (walloc_din[j],walloc_cgroup[j][30:0],walloc_cgroup[j+1][30:0],walloc_c[j],walloc_s[j] );
 	end
 endgenerate
 
