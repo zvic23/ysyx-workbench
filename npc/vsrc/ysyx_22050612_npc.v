5a8c037e397bb6ae56009fa440acb8e149e2852c
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 18:18:24 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:18:24 up  3:30, 10 users,  load average: 0.98, 1.03, 1.00

--- npc/vsrc/ysyx_22050612_npc.v
+++ npc/vsrc/ysyx_22050612_npc.v
@@ -22,7 +22,7 @@ assign mul_valid = 1'b1;
 wire mulw;
 assign mulw = 1'b0;
 wire [1:0]mul_signed;
-assign mul_signed = 2'b01;
+assign mul_signed = 2'b00;
 reg [63:0]mulcand;
 reg [63:0]muler;
 
@@ -45,10 +45,10 @@ always @(posedge clk)begin
 end
 
 wire [127:0]result_r;
-//assign result_r = mulcand * muler;
+assign result_r = mulcand * muler;
 //assign result_r = $signed(mulcand) * $signed(muler);
 //assign result_r = mulcand * $signed(muler);
-assign result_r = $signed(mulcand) * muler;
+//assign result_r = $signed(mulcand) * muler;
 always @(negedge clk)begin
 	if(result_r != {result_hi,result_lo}) begin
 		$display("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!\n");
