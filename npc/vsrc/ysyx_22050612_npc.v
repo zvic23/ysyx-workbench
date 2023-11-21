746d09de46c0f2647614721742d6ffb14a582514
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 14:54:59 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:59 up 58 min,  8 users,  load average: 0.58, 0.34, 0.25

--- npc/vsrc/ysyx_22050612_npc.v
+++ npc/vsrc/ysyx_22050612_npc.v
@@ -22,7 +22,7 @@ assign mul_valid = 1'b1;
 wire mulw;
 assign mulw = 1'b0;
 wire [1:0]mul_signed;
-assign mul_signed = 2'b11;
+assign mul_signed = 2'b00;
 reg [63:0]mulcand;
 reg [63:0]muler;
 
@@ -45,7 +45,8 @@ always @(posedge clk)begin
 end
 
 wire [127:0]result_r;
-assign result_r = $signed(mulcand) * $signed(muler);
+assign result_r = mulcand * muler;
+//assign result_r = $signed(mulcand) * $signed(muler);
 always @(negedge clk)begin
 	$display("mulresult:%h%h",result_hi,result_lo);
 	$display("mulresu  :%h  ",result_r);
