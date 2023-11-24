de965d7750514c17b34420bb5c775251767b8de1
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-24 17:54:10 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:54:10 up 12:10, 11 users,  load average: 0.62, 0.31, 0.21

--- npc/vsrc/ysyx_22050612_EXU.v
+++ npc/vsrc/ysyx_22050612_EXU.v
@@ -1236,7 +1236,7 @@ wire [63:0]divisor ;
 assign dividend  = diving ? src1 : 64'b0;
 assign divisor   = diving ? src2 : 64'b0;
 
-ysyx_22050612_divider boothdiv ((clk&&((opcode == 24'h1d000)||(opcode == 24'h25000))), rst, div_valid, div_flush, divw, div_signed, dividend, divisor, div_ready, div_out_valid, quotient, remainder,stimes);      //the clk has been "&&" with "div divw" opcode to close the clock gating(gate), it can speed up the sidivating.
+ysyx_22050612_divider boothdiv ((clk), rst, div_valid, div_flush, divw, div_signed, dividend, divisor, div_ready, div_out_valid, quotient, remainder,stimes);      //the clk has been "&&" with "div divw" opcode to close the clock gating(gate), it can speed up the sidivating.
 
 
 
