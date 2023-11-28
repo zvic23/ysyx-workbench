edd51036b16e6800805d388a9286ec417266a0f6
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-28 16:43:19 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:43:19 up 15:35, 14 users,  load average: 1.07, 0.66, 0.45

--- npc/vsrc/ysyx_22050612_ALU.v
+++ npc/vsrc/ysyx_22050612_ALU.v
@@ -17,10 +17,9 @@ wire [63:0]sll_result;
 wire [63:0]srl_result;
 wire [63:0]sra_result;
 
-wire [63:0]C;
-assign C= (mode==8'd1)? -B :B;
-//assign C= (mode==8'd1)?(~B+64'b1):B;
-assign add_sub_result = A + C;
+wire [63:0]B_negetive;
+assign B_negetive= (mode==8'd1)? -B :B;
+assign add_sub_result = A + B_negetive;
 assign slt_result = ($signed(A) < $signed(B))? 64'b1:64'b0;
 assign sltu_result = (A < B)? 64'b1:64'b0;
 assign sll_result = (A <<  B);
