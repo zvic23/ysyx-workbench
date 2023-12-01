b960638e99767ca6e10204c6c2e8ab58c9152412
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-01 10:41:40 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:41:40 up  1:08,  9 users,  load average: 1.75, 1.31, 0.88

--- npc/vsrc/ysyx_22050612_PART_PRODUCT.v
+++ npc/vsrc/ysyx_22050612_PART_PRODUCT.v
@@ -4,7 +4,7 @@ module ysyx_22050612_PART_PRODUCT(
 	output [131:0]p,
 	output c
 );
-/*
+
 ///y+1,y,y-1///
 wire y_add,y,y_sub;
 assign y_add = y_in[2];
@@ -21,7 +21,6 @@ assign sel_double_positive = ~y_add &  y &  y_sub;
 wire [131:0]x_sub;
 assign x_sub = x<<1;
 
-assign p = ~(~({132{sel_negative}} & ~x) & ~({132{sel_double_negative}} & ~x_sub) & ~({132{sel_positive}} & x ) & ~({132{sel_double_positive}} &  x_sub));
+//assign p = ~(~({132{sel_negative}} & ~x) & ~({132{sel_double_negative}} & ~x_sub) & ~({132{sel_positive}} & x ) & ~({132{sel_double_positive}} &  x_sub));
 assign c = (sel_negative || sel_double_negative);
-*/
 endmodule
