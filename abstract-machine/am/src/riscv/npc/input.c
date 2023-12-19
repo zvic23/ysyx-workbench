4c03bc4d77ebfe32524225aba8913e2e20ec203b
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-19 16:52:04 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:04 up 16:43, 15 users,  load average: 0.30, 0.14, 0.24

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -4,7 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-
+  uint16_t *KBD_ADDR = (uint16_t*)0xa0000060;
+  uint16_t keyvalue = KBD_ADDR[0];
+  if( (keyvalue & KEYDOWN_MASK) == 0x8000) kbd->keydown = 1;
+  else kbd->keydown = 0;
+  kbd->keycode = keyvalue & ~KEYDOWN_MASK;
   //if(keyvalue)printf("am key:%x\n",keyvalue);
 
 //  kbd->keydown = 0;
