4a35602d7a953aab5e9baa1a250d84a6c2d4e1d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-22 16:31:37 +0800

    > compile NEMU ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:31:37 up  2:47,  8 users,  load average: 0.02, 0.05, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       //case 0xb: ev.event = EVENT_SYSCALL; break;
-      case 0xb: ev.event = EVENT_YIELD; break;
+	    case 0xb: {ev.event = EVENT_YIELD;printf("cause:%x",c->mcause);} break;
       default: ev.event = EVENT_ERROR; break;
     }
 
