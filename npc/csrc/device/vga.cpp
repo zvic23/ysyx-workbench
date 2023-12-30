952ef3ccc83cbb82a6301bda9edfa0e16b6e0b74
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-30 11:58:48 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:48 up 23:33, 18 users,  load average: 0.22, 0.29, 0.61

--- npc/csrc/device/vga.cpp
+++ npc/csrc/device/vga.cpp
@@ -20,7 +20,6 @@ static uint32_t screen_size() {
 
 uint8_t vmem[400*300*4];
 static uint32_t *vgactl_port_base = NULL;
-//#ifdef CONFIG_VGA_SHOW_SCREEN
 #include <SDL2/SDL.h>
 
 static SDL_Renderer *renderer = NULL;
@@ -52,32 +51,15 @@ void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 
-  //if(io_read(AM_GPU_STATUS).ready != 0){
-  //        update_screen();
-  //        io_write(AM_GPU_STATUS,0);
-  //}
-
-  //printf("port:%d\n",(uint32_t)(vgactl_port_base[0]));
+  //printf("port:%d\n",(vgactl_port));
   if( vgactl_port == 1){
 	  update_screen();
 	  vgactl_port = 0;
   }
 }
-//#endif
 
 void init_vga() {
-//  vgactl_port_base = (uint32_t *)new_space(8);
-//  vgactl_port_base[0] = (screen_width() << 16) | screen_height();
-//#ifdef CONFIG_HAS_PORT_IO
-//  add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
-//#else
-//  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
-//#endif
 
-//  vmem = new_space(screen_size());
-//  add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
-//  IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
-//  IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 	if(1){
 		init_screen();
 		memset(&vmem,0,screen_size());
