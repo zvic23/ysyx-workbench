118daefb23b7f1d20ef6e5e3620e536acd0c92a7
tracer-ysyx2204 <tracer@ysyx.org>
2024-01-11 10:39:48 +0800

    > sim RTL ysyx_22050612 张圣力 Linux zsl-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:48 up  1:58,  6 users,  load average: 0.17, 0.17, 0.12

--- npc/csrc/cpu/npc-pipeline-trace.cpp
+++ npc/csrc/cpu/npc-pipeline-trace.cpp
@@ -15,21 +15,6 @@ extern Vysyx_22050612_npc* top;
 #ifdef CONFIG_PIPELINETRACE
 long long A[20],B[20],C[20],D[20],E[20],F[20],G[20],H[20],I[20],J[20],K[20],L[20],M[20],N[20],O[20],P[20];
 void IFU_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
-	/*
-	long long z[19];
-	memcpy(z,A,152);
-	memcpy(&A[1],z,152);
-	A[0] = a;
-	memcpy(z,B,152);
-	memcpy(&B[1],z,152);
-	B[0] = b;
-	memcpy(z,C,152);
-	memcpy(&C[1],z,152);
-	C[0] = c;
-	memcpy(z,D,152);
-	memcpy(&D[1],z,152);
-	D[0] = d;
-*/
 	long long z[19];
 	memcpy(z,A,152);
 	memcpy(&A[1],z,152);
@@ -72,21 +57,6 @@ void IFU_state_trace (long long a,long long b,long long c,long long d,long long 
 
 long long A1[20],B1[20],C1[20],D1[20],E1[20],F1[20],G1[20],H1[20],I1[20],J1[20],K1[20],L1[20],M1[20],N1[20],O1[20],P1[20];
 void IDU_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
-/*
-	long long z[19];
-	memcpy(z,A1,152);
-	memcpy(&A1[1],z,152);
-	A1[0] = a;
-	memcpy(z,B1,152);
-	memcpy(&B1[1],z,152);
-	B1[0] = b;
-	memcpy(z,C1,152);
-	memcpy(&C1[1],z,152);
-	C1[0] = c;
-	memcpy(z,D1,152);
-	memcpy(&D1[1],z,152);
-	D1[0] = d;
-*/
 	long long z[19];
 	memcpy(z,A1,152);
 	memcpy(&A1[1],z,152);
@@ -128,27 +98,6 @@ void IDU_state_trace (long long a,long long b,long long c,long long d,long long 
 
 long long A2[20],B2[20],C2[20],D2[20],E2[20],F2[20],G2[20],H2[20],I2[20],J2[20],K2[20],L2[20],M2[20],N2[20],O2[20],P2[20];
 void EXU_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
-/*
-	long long z[19];
-	memcpy(z,A2,152);
-	memcpy(&A2[1],z,152);
-	A2[0] = a;
-	memcpy(z,B2,152);
-	memcpy(&B2[1],z,152);
-	B2[0] = b;
-	memcpy(z,C2,152);
-	memcpy(&C2[1],z,152);
-	C2[0] = c;
-	memcpy(z,D2,152);
-	memcpy(&D2[1],z,152);
-	D2[0] = d;
-	memcpy(z,E2,152);
-	memcpy(&E2[1],z,152);
-	E2[0] = e;
-	memcpy(z,F2,152);
-	memcpy(&F2[1],z,152);
-	F2[0] = f;
-*/
 	long long z[19];
 	memcpy(z,A2,152);
 	memcpy(&A2[1],z,152);
@@ -190,27 +139,6 @@ void EXU_state_trace (long long a,long long b,long long c,long long d,long long 
 
 long long A3[20],B3[20],C3[20],D3[20],E3[20],F3[20],G3[20],H3[20],I3[20],J3[20],K3[20],L3[20],M3[20],N3[20],O3[20],P3[20];
 void MEM_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
-	/*
-	long long z[19];
-	memcpy(z,A3,152);
-	memcpy(&A3[1],z,152);
-	A3[0] = a;
-	memcpy(z,B3,152);
-	memcpy(&B3[1],z,152);
-	B3[0] = b;
-	memcpy(z,C3,152);
-	memcpy(&C3[1],z,152);
-	C3[0] = c;
-	memcpy(z,D3,152);
-	memcpy(&D3[1],z,152);
-	D3[0] = d;
-	memcpy(z,E3,152);
-	memcpy(&E3[1],z,152);
-	E3[0] = e;
-	memcpy(z,F3,152);
-	memcpy(&F3[1],z,152);
-	F3[0] = f;
-	*/
 	long long z[19];
 	memcpy(z,A3,152);
 	memcpy(&A3[1],z,152);
@@ -252,27 +180,6 @@ void MEM_state_trace (long long a,long long b,long long c,long long d,long long 
 
 long long A4[20],B4[20],C4[20],D4[20],E4[20],F4[20],G4[20],H4[20],I4[20],J4[20],K4[20],L4[20],M4[20],N4[20],O4[20],P4[20];
 void WBU_state_trace (long long a,long long b,long long c,long long d,long long e,long long f,long long g,long long h,long long i,long long j,long long k,long long l,long long m,long long n,long long o,long long p){
-/*
-	long long z[19];
-	memcpy(z,A4,152);
-	memcpy(&A4[1],z,152);
-	A4[0] = a;
-	memcpy(z,B4,152);
-	memcpy(&B4[1],z,152);
-	B4[0] = b;
-	memcpy(z,C4,152);
-	memcpy(&C4[1],z,152);
-	C4[0] = c;
-	memcpy(z,D4,152);
-	memcpy(&D4[1],z,152);
-	D4[0] = d;
-	memcpy(z,E4,152);
-	memcpy(&E4[1],z,152);
-	E4[0] = e;
-	memcpy(z,F4,152);
-	memcpy(&F4[1],z,152);
-	F4[0] = f;
-	*/
 	long long z[19];
 	memcpy(z,A4,152);
 	memcpy(&A4[1],z,152);
